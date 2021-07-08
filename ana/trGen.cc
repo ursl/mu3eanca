e4048f2aea5104dff27bc42311471eb3bc496487
Urs Langenegger <urslangenegger@gmail.com>
2021-07-08 16:35:57 +0200

    added simplistic conversion plots

--- ana/trGen.cc
+++ ana/trGen.cc
@@ -15,6 +15,38 @@ using namespace std;
 // ----------------------------------------------------------------------
 
 
+// -- 2 Example events:
+//    Note: vertex gives particle origin
+//          type 41,42 are positron, electron from conversions
+//
+// ----------------------------------------------------------------------
+// mu3e evt: 10 event: 90 run: 779 type: -1: fNtrajectories = 5:  : fNhit = 4:
+// trj  0 ID =    4159 PID =  -13 type =   3 mother ID =       0 vz = -1000.000 vr =   +5.688 time = 34.589
+// trj  1 ID =    4163 PID =  -13 type =   3 mother ID =       0 vz = -1000.000 vr =   +5.281 time = 13.2837
+// trj  2 ID =    4170 PID =  -13 type =   3 mother ID =       0 vz = -1000.000 vr =  +10.193 time = 45.3868
+// trj  3 ID =    4092 PID =  -13 type =   3 mother ID =       0 vz = -1000.000 vr =   +2.402 time = -16.2756
+// trj  4 ID =    1425 PID =  -11 type =  11 mother ID =    1416 vz =   +62.250 vr =  +31.640 time = 46.5969
+// hit   0 pxhitid = 4525760 timestamp = 1429 hit_mc_i =   81 hit_mc_n =    1
+// hit   1 pxhitid = 19239643 timestamp = 1432 hit_mc_i =   82 hit_mc_n =    1
+// hit   2 pxhitid = 23329223 timestamp = 1425 hit_mc_i =   83 hit_mc_n =    1
+// hit   3 pxhitid = 86360979 timestamp = 1432 hit_mc_i =   84 hit_mc_n =    1
+// ----------------------------------------------------------------------
+//
+// .. snip ..
+//
+//  ----------------------------------------------------------------------
+// mu3e evt: 13 event: 93 run: 779 type: -1: fNtrajectories = 9:  : fNhit = 4:
+// trj  0 ID =    4375 PID =  -13 type =   3 mother ID =       0 vz = -1000.000 vr =  +14.799 time = 34.4954
+// trj  1 ID =    4169 PID =  -11 type =  11 mother ID =    4163 vz =   -34.456 vr =   +5.889 time = 6.11924
+// trj  2 ID =    4381 PID =  +22 type =  60 mother ID =    4169 vz =  +313.954 vr =  +34.922 time = 8.80184
+// trj  3 ID =    4407 PID =  -11 type =  41 mother ID =    4381 vz =  +505.057 vr = +507.920 time = 10.7073
+// trj  4 ID =    4406 PID =  +11 type =  42 mother ID =    4381 vz =  +505.057 vr = +507.920 time = 10.7073
+// trj  5 ID =    3903 PID =  -11 type =  11 mother ID =    3888 vz =   +35.351 vr =   +5.547 time = 34.3446
+// trj  6 ID =    4439 PID =  +22 type =  60 mother ID =    3903 vz =  -178.997 vr =  +30.122 time = 36.5057
+// trj  7 ID =    4456 PID =  +11 type =  82 mother ID =    4439 vz =  -444.659 vr = +506.566 time = 38.5018
+// trj  8 ID =    1886 PID =  -11 type =  11 mother ID =    1876 vz =   +21.899 vr =  +10.668 time = 22.5846
+
+
 // Mu3eEvent.cc:
 // if(ptype >= 0 && dtype >= 0) trajType = 10 * dtype + ptype;
 
@@ -84,21 +116,30 @@ void trGen::eventProcessing() {
   }
 
   fillHist();
-  michelSpectrum();
+  genStudy();
 
 
 }
 
 
 // ----------------------------------------------------------------------
-void trGen::michelSpectrum() {
+void trGen::genStudy() {
 
-  TH1D *h1 = (TH1D*)fpHistFile->Get("hmichel");
   TLorentzVector p4;
   for (unsigned int i = 0; i < fNtrajectories; ++i) {
+    // -- Michel decay electrons
     if ((-11 == ftraj_PID->at(i)) && (11 == ftraj_type->at(i))) {
       p4.SetXYZM(ftraj_px->at(i), ftraj_py->at(i), ftraj_pz->at(i), MMUON);
-      h1->Fill(p4.Rho());
+      double r = TMath::Sqrt(ftraj_vx->at(i)*ftraj_vx->at(i) + ftraj_vy->at(i)*ftraj_vy->at(i));
+      ((TH1D*)fpHistFile->Get("hmichel"))->Fill(p4.Rho());
+      ((TH2D*)fpHistFile->Get("vrzmichel"))->Fill(ftraj_vz->at(i), r);
+      ((TH2D*)fpHistFile->Get("vxymichel"))->Fill(ftraj_vx->at(i), ftraj_vy->at(i));
+    }
+    // -- converted photons
+    if ((-11 == ftraj_PID->at(i)) && (41 == ftraj_type->at(i))) {
+      double r = TMath::Sqrt(ftraj_vx->at(i)*ftraj_vx->at(i) + ftraj_vy->at(i)*ftraj_vy->at(i));
+      ((TH1D*)fpHistFile->Get("vrzconv"))->Fill(ftraj_vz->at(i), r);
+      ((TH1D*)fpHistFile->Get("vxyconv"))->Fill(ftraj_vx->at(i), ftraj_vy->at(i));
     }
   }
 }
@@ -121,7 +162,11 @@ void trGen::bookHist() {
 
   new TH1D("hpx", "hpx", 100, -100., 100.);
   new TH1D("hmichel", "hmichel", 60, 0., 60.);
-  new TH2D("vmichel", "vmichel (r vs. z)", 100, -200., 200., 100, -200., 200.);
+  new TH2D("vrzmichel", "vmichel (r vs. z)", 200, -1500., 500., 120, 0., 60.);
+  new TH2D("vxymichel", "vmichel (x vs. y)", 100, -100., 100., 100, -100., 100.);
+
+  new TH2D("vrzconv", "vconv (r vs. z)", 200, -1500., 500., 120, 0., 60.);
+  new TH2D("vxyconv", "vconv (x vs. y)", 100, -100., 100., 100, -100., 100.);
 
 }
 
