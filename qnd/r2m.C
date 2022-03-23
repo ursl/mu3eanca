5f09429c85478ad3969ef37b0c2a3f3b2f76a890
Urs Langenegger <urslangenegger@gmail.com>
2022-03-23 17:12:25 +0100

    update

--- qnd/r2m.C
+++ qnd/r2m.C
@@ -43,10 +43,12 @@ void v0() {
   
   // -- check parsed JSON file
   int intRunChip(-1), simChip(-1);
+  map<int, int> simChip2irChip;
   for (json::iterator it = jMap.begin(); it != jMap.end(); ++it) {
     it->at("intRunChip").get_to(intRunChip);
     it->at("simChip").get_to(simChip);
     cout << "intRunChip = " << intRunChip << " simChip = " << simChip << endl;
+    simChip2irChip[simChip] = intRunChip;
   }
 
   // -- get trees
@@ -65,8 +67,9 @@ void v0() {
   tMu3e->SetBranchAddress("hit_timestamp", &v_hit_timestamp, &b_hit_timestamp);
 
 
-  int nmax(20);
+  int nmax(100);
   // nmax = tMu3e->GetEntries();
+  int icol(-1), irow(-1), isen(-1);
   for (int ievt = 0; ievt < nmax; ++ievt) {
     Long64_t tentry = tMu3e->LoadTree(ievt);
     b_hit_pixelid->GetEntry(tentry); 
@@ -76,13 +79,19 @@ void v0() {
     cout << Form("%5d", ievt)
 	 << " Nhit = " << Nhit << ", pixelid = "; 
     for (int ihit = 0; ihit < Nhit; ++ihit) {
-      //      int sensorID = v_hit_pixelid->at(ihit) & 0xff
-      cout << hex << v_hit_pixelid->at(ihit) << " " << dec;
+      unsigned int pixelid = v_hit_pixelid->at(ihit);
+      irow = pixelid & 0xff;
+      icol = (pixelid >> 8) & 0xff;
+      isen = (pixelid >> 16) & 0xffff;
+      cout << hex << pixelid << " " << dec;
+      cout << " (" << isen << " aka " << simChip2irChip[isen] << ": " << icol << "/" << irow << ")";
+      cout << "  ";
     }
-    cout << " ts = ";
-    for (int ihit = 0; ihit < Nhit; ++ihit) {
-      cout << v_hit_timestamp->at(ihit) << " ";
-    }
+    // cout << " ts = ";
+    // for (int ihit = 0; ihit < Nhit; ++ihit) {
+    //   cout << v_hit_timestamp->at(ihit) << " ";
+    // }
+
     cout << endl;
   }
 
