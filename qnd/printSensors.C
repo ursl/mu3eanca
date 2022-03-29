d7746326bff35acb2047a542d0c1af51b2ac93f7
Urs Langenegger <urslangenegger@gmail.com>
2022-03-29 09:07:51 +0200

    futile attempts to calculate numbers

--- qnd/printSensors.C
+++ qnd/printSensors.C
@@ -8,7 +8,8 @@ void printSensors(string fname = "run000042-sort_100k.root") {
 
   TTree *ts = (TTree*)gFile->Get("alignment/sensors");
   //  ts->Print();
-  unsigned int sensor(0), mysensor(0);
+  unsigned int sensor(0);
+  int mysensor(0), mySimChipId(0), ldrmax(0);
   unsigned int chip(0), ladder(0), layer(0), rest(0); 
   double vx(0.), vy(0.), vz(0.);
   
@@ -18,6 +19,8 @@ void printSensors(string fname = "run000042-sort_100k.root") {
   ts->SetBranchAddress("vz", &vz);
 
   Long64_t nentries = ts->GetEntries();
+  cout << "Idx  algn/sensorId (mySimId)  .. etc" << endl;
+
   for (int ievt = 0; ievt < nentries; ++ievt) {
     ts->GetEntry(ievt);
     TVector3 r(vx, vy, vz);
@@ -27,10 +30,20 @@ void printSensors(string fname = "run000042-sort_100k.root") {
     layer  = (sensor >> 10) & 0x1f;
     rest   = (sensor >> 15);
 
-    mysensor = layer*1024 + ladder*32 + chip;
+    mysensor    = layer*1024 + ladder*32 + chip;
+    if (0 == layer) {
+      ldrmax = 8;
+      if (ladder > 4) ladder -= 4; 
+    } else if (1 == layer) {
+      if (ladder > 5) ladder -= 2; 
+      ldrmax = 12;
+    } else {
+      ldrmax = 1000;
+    }
+    mySimChipId = layer*48   + ladder*6 + (6-chip);
     
-    cout << Form("%3d: sensor = %4d (%4d) chip/ldr/lyr = %2d/%2d/%2d r = %5.2f phi = %6.1f z = %+7.3f  x/y = %+8.4f/%+8.4f",
-		 ievt, sensor, mysensor, chip, ladder, layer,
+    cout << Form("%3d: sensor = %4d (%4d) chip/ldr/lyr = %2d/%2d/%2d r = %5.2f phi = %6.1f z = %+7.3f x/y = %+8.4f/%+8.4f",
+		 ievt, sensor, mySimChipId, chip, ladder, layer,
 		 TMath::Sqrt(vx*vx + vy*vy), r.Phi()*TMath::RadToDeg(), vz,
 		 vx, vy)
 	 << endl;
