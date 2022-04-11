c799b2ac4d74608f4f2fae8ed6223b89c81c88b9
Urs Langenegger <urslangenegger@gmail.com>
2022-04-11 18:37:11 +0200

    first version of IV JSON analysis

---
+++ qnd/ladderTests.C
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../../analyzer/analyzer/utility/json.h"
+
+using namespace std;
+
+// ----------------------------------------------------------------------
+string readFromJson(string filename, vector<string> what) {
+  ifstream INS(filename);
+  string sline;
+  vector<bool> found;
+  int cnt(0); 
+  for (unsigned int i = 0; i < what.size(); ++i) {
+    found.push_back(false);
+  }
+  while (getline(INS, sline)) {
+    //    cout << sline << endl;
+    string search = string("\"") + what[cnt] + string("\"");
+    if (string::npos != sline.find(search)) {
+      ++cnt;
+    }
+    if (cnt == what.size()) {
+      cout << "==return> " << sline << endl;
+      break;
+    }
+  }
+  return sline; 
+}
+
+// ----------------------------------------------------------------------
+vector<pair<double, double> > combine2Lines(string l1, string l2) {
+  vector<pair<double, double> > result;
+
+  l1 = l1.substr(l1.find("[") + 2);
+  l1 = l1.substr(0, l1.rfind("]") -1);
+  l2 = l2.substr(l2.find("[") + 2);
+  l2 = l2.substr(0, l2.rfind("]") -1);
+
+  cout << "l1 ->" << l1 << "<-" << endl;
+  cout << "l2 ->" << l2 << "<-" << endl;
+  
+  // -- start reading until
+  double val1, val2;
+  string comma;
+  istringstream sl1(l1);
+  istringstream sl2(l2);
+  sl1 >> val1 >> comma;
+  sl2 >> val2 >> comma;
+  cout << "val1(" << val1 << "), val2(" << val2 << ")" << endl;
+  result.push_back(make_pair(val1, val2));
+  
+  while (1) {
+    sl1 >> val1 >> comma;
+    sl2 >> val2 >> comma;
+    if (val1 < 1) break;
+    result.push_back(make_pair(val1, val2));
+    cout << result.size() << ": val1(" << val1 << "), val2(" << val2 << ")" << endl;
+  }
+  
+  
+  return result;
+  
+}
+
+
+// ----------------------------------------------------------------------
+TH1D* ivTest(string filename= "qc_ladder_0.json") {
+  cout << "reading json from file ->" << filename << "<-" << endl;
+  vector<string> ivstrings;
+  ivstrings.push_back("IV");
+  ivstrings.push_back("Output");
+  ivstrings.push_back("Voltage");
+  string voltage = readFromJson(filename, ivstrings); 
+  ivstrings.clear();
+  ivstrings.push_back("IV");
+  ivstrings.push_back("Output");
+  ivstrings.push_back("Current");
+  string current = readFromJson(filename, ivstrings); 
+
+  vector<pair<double, double> > result = combine2Lines(voltage, current); 
+
+  TH1D *h1 = new TH1D("h1", Form("IV curve %s", filename.c_str()), 30, 0., 30.);
+  h1->SetMaximum(0.5);
+  h1->SetMinimum(-12.0);
+  for (unsigned int i = 0; i < h1->GetNbinsX(); ++i) {
+    h1->SetBinContent(i+1, 1.0);
+  }
+  
+  h1->SetMarkerStyle(20);
+  h1->SetMarkerSize(0.8);
+  h1->GetXaxis()->SetTitle("Bias voltage [-V]");
+  h1->GetYaxis()->SetTitle("Current [#mu A]");
+  h1->GetYaxis()->SetTitleOffset(1.1);
+  for (unsigned int i = 0; i < result.size(); ++i) {
+    cout << "Filling " << h1->FindBin(result[i].first) << " " <<  result[i].second << endl;
+    h1->SetBinContent(h1->FindBin(result[i].first), result[i].second);
+  }
+
+  h1->Draw("p0");
+  return h1; 
+}
+
+
+// ----------------------------------------------------------------------
+void ivTests(int layer = 1) {
+  vector<string> files;
+  vector<int> cols;
+  
+  if (0 == layer) {
+    files.push_back("qc_ladder_0.json"); cols.push_back(kBlack);
+    files.push_back("qc_ladder_1.json"); cols.push_back(kBlue);
+    files.push_back("qc_ladder_2.json"); cols.push_back(kRed); 
+    files.push_back("qc_ladder_3.json"); cols.push_back(kCyan); 
+    files.push_back("qc_ladder_4.json"); cols.push_back(kMagenta); 
+    files.push_back("qc_ladder_6.json"); cols.push_back(kGreen+1); 
+    files.push_back("qc_ladder_7.json"); cols.push_back(kBlue+2);   
+  };
+
+  
+  if (1 == layer) {
+    files.push_back("qc_ladder_10.json"); cols.push_back(kBlack);    
+    files.push_back("qc_ladder_11.json"); cols.push_back(kBlue);     
+    files.push_back("qc_ladder_12.json"); cols.push_back(kRed);      
+    files.push_back("qc_ladder_13.json"); cols.push_back(kCyan);     
+    files.push_back("qc_ladder_14.json"); cols.push_back(kMagenta);  
+    files.push_back("qc_ladder_15.json"); cols.push_back(kGreen+1);  
+    files.push_back("qc_ladder_16.json"); cols.push_back(kBlue+2);   
+    files.push_back("qc_ladder_17.json"); cols.push_back(kRed+2);   
+  };
+  vector<string> opts;
+  vector<string> titles;
+  for (unsigned int i = 0; i < files.size(); ++i) {
+    opts.push_back("p");
+    string title = files[i];
+    replaceAll(title, ".json", "");
+    replaceAll(title, "qc_", "");
+    replaceAll(title, "_", " ");
+    titles.push_back(title);
+  }
+  
+  vector<TH1*> hists; 
+  for (unsigned int i = 0; i < files.size(); ++i) {
+    hists.push_back(ivTest(files[i]));
+    hists[i]->SetMarkerColor(cols[i]);
+  }
+
+  gStyle->SetOptStat(0); 
+  gStyle->SetOptTitle(0); 
+  
+  for (unsigned int i = 0; i < hists.size(); ++i) {
+    if (0 == i) {
+      hists[i]->Draw("p0");
+    } else {
+      hists[i]->Draw("p0same");
+    }
+  }
+
+  TLegend *tll = newLegend("Half ladders", 0.7, 0.2, 0.88, 0.5, hists, titles, opts);
+  tll->SetTextSize(0.03);
+  tll->Draw();
+
+  if (1 == layer) {
+    c0.SaveAs("iv-layer1.pdf");
+  }
+  if (0 == layer) {
+    c0.SaveAs("iv-layer0.pdf");
+  }
+  
+}
+
+// // ----------------------------------------------------------------------
+// int main() {
+//   iv();
+//   return 0;
+// }
