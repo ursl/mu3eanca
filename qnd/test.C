75443b3892ea39f70aa192ff5a3efb4257424f87
Urs Langenegger <urslangenegger@gmail.com>
2022-05-31 16:23:49 +0200

    this works on merlin, w/o my own confusing json

--- qnd/test.C
+++ qnd/test.C
@@ -2,14 +2,10 @@
 #include "../common/json.h"
 //#include "nlohmann/json.hpp"
 
-//using json = nlohmann::json;
-
-nlohmann::json jMap;
-
 void test() {
   
   ifstream i("../common/sensors_mapping_220531.json");
-  //  nlohmann::json jMap;
+  nlohmann::json jMap;
   i >> jMap;
    
 }
