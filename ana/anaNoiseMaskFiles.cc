ca83130f44b1c14eb4f2679ae293e5149e4ad20c
Urs Langenegger <urslangenegger@gmail.com>
2022-06-21 09:24:11 +0200

    add error code printout

--- ana/anaNoiseMaskFiles.cc
+++ ana/anaNoiseMaskFiles.cc
@@ -10,15 +10,13 @@
 #include "util/util.hh"
 #include "../common/json.h"
 
-
-
-
 using namespace::std;
 
 map<int, struct sensor> gDetectorChips;
 map<int, vector<pair<int, int> > > gChipNoisyPixels; 
 
 int VERBOSE(0); 
+int NCHIP(120);
 
 // ----------------------------------------------------------------------
 int getValInt(string line) {
@@ -194,8 +192,10 @@ int idxFromColRow(int col, int row) {
 // ----------------------------------------------------------------------
 void summarize(vector<uint8_t> vnoise) {
   int cntNoisy(0);
+  int errCode(0);
   for (unsigned int i = 0; i < vnoise.size(); ++i){
     if ((0xda == vnoise[i]) && (0xda == vnoise[i+1])) {
+      errCode = vnoise[i+5];
       i += 5; //??
       continue;
     }
@@ -206,7 +206,9 @@ void summarize(vector<uint8_t> vnoise) {
     }
   }
   if (VERBOSE > 0) cout  << endl;
-  cout << " Number of noisy pixels = " << cntNoisy << endl;
+  cout << " Number of noisy pixels = " << cntNoisy
+       << (errCode>0? Form(", errCode = %d", errCode): "")
+       << endl;
 }
 
 
@@ -266,13 +268,17 @@ void addNoiseMaskFile(vector<uint8_t> &vnoise, string name) {
   }
   
   for (unsigned int i = 0; i < vread.size(); ++i){
-    pair<int, int> a = colrowFromIdx(i);
     if ((0xda == vnoise[i]) && (0xda == vnoise[i+1])) {
+      // -- update error flag in case it is set
+      if (0x00 != vread[i+5]) {
+        vnoise[i] = vread[i];
+      }
       i += 5; //??
       continue;
     }
  
     if ((0xff == vnoise[i]) && (0xff != vread[i])) {
+      pair<int, int> a = colrowFromIdx(i);
       if (VERBOSE > 1) cout << Form("file %s change setting col/row = %3d/%3d from %x to %x",
                                     name.c_str(), a.first, a.second, vnoise[i], vread[i])
                             << endl;
@@ -284,7 +290,7 @@ void addNoiseMaskFile(vector<uint8_t> &vnoise, string name) {
  
  
 // ----------------------------------------------------------------------
-// -- combines all runs into one mask file and summarizes the combination
+// -- combines files into one mask file and summarizes the combination
 // ----------------------------------------------------------------------
 void mergeNoiseFiles(vector<string> filelist) {
   vector<uint8_t> vnoise;
@@ -298,28 +304,61 @@ void mergeNoiseFiles(vector<string> filelist) {
   summaryMaskFile("combination");
   remove("combination");
 }
- 
+
+// ----------------------------------------------------------------------
+// -- combines files into one mask file and summarizes the combination
+// ----------------------------------------------------------------------
+void mergeRunNoiseFiles(vector<int> runlist, string dir = ".") {
+  vector<uint8_t> vnoise;
+  
+  for (unsigned int ichip = 0; ichip < NCHIP; ++ichip) {
+    // -- initialize with no noisy pixels
+    vnoise.clear();
+    for (int i = 0; i < 256*256; ++i) vnoise.push_back(0xff);
+    // -- add runs
+    for (unsigned int irun = 0; irun < runlist.size(); ++irun) {
+      string filename = Form("%s/noiseMaskFile-run%d-chipID%d", dir.c_str(), runlist[irun], ichip);
+      addNoiseMaskFile(vnoise, filename);
+    }
+    writeNoiseMaskFile(vnoise, Form("%s/noiseMaskFile-chipID%d", dir.c_str(), ichip));
+  }
+}
+
+
 
 // ----------------------------------------------------------------------
 // -- main
 // ----------------------------------------------------------------------
 int main(int argc, char *argv[]) {
+  string outputdir(".");
   // -- command line arguments
   for (int i = 0; i < argc; i++){
+    if (!strcmp(argv[i],"-v"))  {
+      VERBOSE = atoi(argv[++i]);
+    }      
+
+    if (!strcmp(argv[i],"-o"))  {
+      outputdir = argv[++i];
+    }      
+
     if (!strcmp(argv[i],"-h")) {
-      cout << "List of arguments:" << endl;
+      cout << "List of arguments: (provide -v as first argument!)" << endl;
       cout << "-h                prints this message and exits" << endl;
       cout << "-c file1 file2    combine various noisemaskfiles for a single chip" << endl;
+      cout << "-o outputdir      set output directory" << endl;
       cout << "-r run1,run2,run3 combine noisemaskfiles for all chips for given runs" << endl;
       cout << "-s filename       summarize noisemask with filename" << endl;
       cout << "-v level          set verbosity level " << endl;
       return 0;
     }
+  }
 
-    if (!strcmp(argv[i],"-v"))  {
-      VERBOSE = atoi(argv[++i]);
-    }      
+  for (int i = 0; i < argc; i++){
+    // --  previously parsed
+    if (!strcmp(argv[i],"-v"))  {++i; continue;}
+    if (!strcmp(argv[i],"-o"))  {++i; continue;}
 
+    // -- combine specific files
     if (!strcmp(argv[i],"-c"))  {
       vector<string> fnames; 
       for (int j = i+1; j < argc; ++j) {
@@ -329,21 +368,24 @@ int main(int argc, char *argv[]) {
       return 0; 
     }
 
+    // -- combine all files for specific runs
     if (!strcmp(argv[i],"-r"))  {
-      vector<string> runlist = split(argv[++i], ',');
-      for (int j = 0; j < runlist.size(); ++j) {
+      vector<string> srunlist = split(argv[++i], ',');
+      vector<int> runlist;
+      for (int j = 0; j < srunlist.size(); ++j) {
+        runlist.push_back(atoi(srunlist[j].c_str()));
         cout << "j = " << j << " run = " << runlist[j] << endl;
       }
+      mergeRunNoiseFiles(runlist, outputdir);
       return 0; 
     }
 
-    
+    // -- summarize a single noise mask file
     if (!strcmp(argv[i],"-s"))  {
       string filename = argv[++i];
       summaryMaskFile(filename);
       return 0; 
     }
-
 
   }
   return 0;
