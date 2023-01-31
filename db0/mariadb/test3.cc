c4885d71eee1b25cb9c34b70540292f5c9e10d13
Urs Langenegger <urslangenegger@gmail.com>
2023-01-31 18:04:41 +0100

    close to writing

--- db0/mariadb/test3.cc
+++ db0/mariadb/test3.cc
@@ -38,6 +38,9 @@
 #include <iostream>
 #include <system_error>
 
+#include <chrono>
+#include <time.h>
+
 using namespace std;
 
 int loops = 2;
@@ -93,20 +96,86 @@ void executeQuery(std::unique_ptr<sql::Connection> & conn, string cmd = "") {
   std::unique_ptr<sql::Statement> stmt(conn->createStatement());
   sql::ResultSet *res;
 
-  conn->setSchema("Mu3e");
-  res = stmt->executeQuery("select RunNumber, RunDescription, Mu3eSchema, StartTime, EndTime  from RunCollection");
+  std::string SQL = "select RunNumber, Mu3eSchema, StartTime, EndTime, Frames, DataSize, BeamOn, Magnet, RunType from RunCollection";
+
+  res = stmt->executeQuery(SQL);
   cout << "res->rowsCount() = " << res->rowsCount() << endl;
   while (res->next()) {
-    cout << "RunNumber: " << res->getString("RunNumber") << endl;
-    // Access column data by alias or column name 
-    cout << "StartTime: " << res->getString("StartTime") << endl;
-    // Access column data by numeric offset, 1 is the first column 
-    // cout << res->getString(1) << endl;
-    // playing
-    cout << "Mu3eSchema: " << res->getString("Mu3eSchema") << endl;
+    cout << res->getInt(1) << " "
+         << res->getInt(2) << " "
+         << res->getString(3) << " "
+         << res->getString(4) << " "
+         << res->getString(5) << " "
+         << res->getString(6) << " "
+         << res->getString(7) << " "
+         << res->getString(8) << " "
+         << res->getString(9) << " "
+         << endl;
+    //cout << "RunNumber: " << res->getString("RunNumber") << endl;
   }
-  
 }
+
+// ----------------------------------------------------------------------
+void executeWrite(std::unique_ptr<sql::Connection> & conn, int first, int nruns) {
+  unique_ptr<sql::Statement> stmt(conn->createStatement());
+  string SQL0 = "INSERT INTO RunCollection (RunNumber, Mu3eSchema, StartTime, EndTime, Frames, DataSize, BeamOn, Magnet, ShiftCrew, RunDescription, DeliveredCharge, SciCatId, RunType) VALUES ";
+
+  int Mu3eSchema(1001);
+  int BeamOn(1900);
+  int Frames(20100);
+  int DataSize(30000001);
+  int DeliveredCharge(720000);
+  int RunType(22001);
+  float Magnet(1.1);
+  string StartTime, EndTime, ShiftCrew("N. Sense, S. Body"), RunDescription("test"), SciCatId("Mu3e-2023-000567");
+
+  char buff[70];
+  struct tm my_time;
+  my_time.tm_year = 123; // = year 2012
+  my_time.tm_mon  = 0;    // = 10th month
+  my_time.tm_mday = 31;   // = 9th day
+  my_time.tm_hour = 8;   // = 8 hours
+  my_time.tm_min  = 5;   
+  my_time.tm_sec  = 7;   
+
+  for (int irun = first; irun <= first+nruns; ++irun) {
+    DataSize = 700456 + 44*irun;
+    Frames   = 60000 + 7*irun;
+
+    my_time.tm_min += 1;    // add 1 min
+    if (strftime(buff, sizeof buff, "%Y-%m-%d %H:%M:%S", &my_time)) {
+      StartTime = string(buff);
+    }
+    
+    my_time.tm_sec += 2;    // add 2 secs
+    if (strftime(buff, sizeof buff, "%Y-%m-%d %H:%M:%S", &my_time)) {
+      EndTime = string(buff);
+    }
+
+    
+    stringstream s;
+    s << "("
+      << "'" << irun << "', "
+      << "'" << Mu3eSchema << "', "
+      << "'" << StartTime << "', "
+      << "'" << EndTime << "', "
+      << "'" << Frames << "', "
+      << "'" << DataSize << "', "
+      << "'" << BeamOn << "', "
+      << "'" << Magnet << "', "
+      << "'" << ShiftCrew << "', "
+      << "'" << RunDescription << "', "
+      << "'" << DeliveredCharge << "', "
+      << "'" << SciCatId << "', "
+      << "'" << RunType << "'"
+      << ")";
+
+    string SQL1 = s.str();
+    cout << SQL0+SQL1 << endl;
+  }
+}
+
+
 
 
 // ----------------------------------------------------------------------
@@ -139,10 +208,25 @@ int main(int argc, const char **argv) {
   int last_error_total = 0;
   int i;
 
-  const std::string host(argc >=2 ? argv[1] : HOST_ENV_OR_DEFAULT);
-  const std::string user(argc >=3 ? argv[2] : UID_ENV_OR_DEFAULT);
-  const std::string pass(argc >=4 ? argv[3] : PASSWD_ENV_OR_DEFAULT);
-  const std::string database(argc >=5 ? argv[4] : SCHEMA_ENV_OR_DEFAULT);
+  // -- command line arguments
+  bool doRead(false), doWrite(false);
+  int first(-1), nruns(-1);
+  string pass;
+  for (int i = 0; i < argc; i++){
+    if (!strcmp(argv[i],"-f"))  {first   = atoi(argv[++i]);}
+    if (!strcmp(argv[i],"-n"))  {nruns   = atoi(argv[++i]);}
+    if (!strcmp(argv[i],"-r"))  {doRead  = true;}
+    if (!strcmp(argv[i],"-w"))  {doWrite = true;}
+    if (!strcmp(argv[i],"-p"))  {pass    = argv[++i];}
+  }
+
+
+  // -- Configure Connection
+
+  string host("127.0.0.1");
+  string user("root");
+  string database("Mu3e");
+
   const bool useTls= USETLS_ENV_OR_DEFAULT;
   
   std::cout << "# Host=" << host << std::endl;
@@ -167,7 +251,9 @@ int main(int argc, const char **argv) {
     printf("not ok\n");
     return 1;
   }
-  
+
+  conn->setSchema(database);
+ 
   if (!conn->isValid(10)) {
     printf("\n# ERR: Connection is not valid at %s::%d\n", CPPCONN_FUNC, __LINE__);
     printf("not ok\n");
@@ -176,7 +262,15 @@ int main(int argc, const char **argv) {
     std::cout << "all is well" << std::endl;
   }
 
-  executeQuery(conn, "");
+  if (doWrite) {
+    executeWrite(conn, first, nruns);
+  }
+  
+  if (doRead) {
+    executeQuery(conn, "");
+  }
+
+  
   
   return 0;
 }
