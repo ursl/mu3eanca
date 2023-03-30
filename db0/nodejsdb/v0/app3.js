69e350e24f2a3f718d1665c321ec1de3972696bf
Urs Langenegger <urslangenegger@gmail.com>
2023-03-30 10:53:10 +0200

    add module

--- db0/nodejsdb/v0/app3.js
+++ db0/nodejsdb/v0/app3.js
@@ -4,45 +4,26 @@
 // [edit local.json with username, password, host, and port]
 // node app3.js
 // ----------------------------------------------------------------------
+const http = require('http');
 var config = require('config');
+
+var dt = require('./myfirstmodule');
+
 
 var dbhoststring = 'http://' + config.get('db.username') + ":" + config.get('db.password')
     + "@" + config.get('db.host') + ":" + config.get('db.port')
 var nano = require('nano')(dbhoststring);
 
-// clean up the database we created previously
-nano.db.destroy('alice', function() {
-    // create a new database
-    nano.db.create('alice', function() {
-        // specify the database we are going to use
-        var alice = nano.use('alice');
-        // and insert a document in it
-        alice.insert({ crazy: true }, 'rabbit', function(err, body, header) {
-            if (err) {
-                console.log('[alice.insert] ', err.message);
-                return;
-            }
-            console.log('you have inserted the rabbit.')
-            console.log(body);
-        });
 
-        alice.insert({ crazy: true }, 'mouse', function(err, body, header) {
-            if (err) {
-                console.log('[alice.insert] ', err.message);
-                return;
-            }
-            console.log('you have inserted the mouse.')
-            console.log(body);
-        });
+const hostname = '127.0.0.1';
+const port = 3000;
 
-        alice.insert({ crazy: true }, 'badger', function(err, body, header) {
-            if (err) {
-                console.log('[alice.insert] ', err.message);
-                return;
-            }
-            console.log('you have inserted the badger.')
-            console.log(body);
-        });
-        
-    });
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!!!! date and time: '+ dt.myDateTime());
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
