5e75b6e8912f9846ae8e78cd3a8ba3df3dbc3852
Urs Langenegger <urslangenegger@gmail.com>
2023-04-19 22:28:01 +0200

    confused

--- db0/nodejsdb/express-couch/public/client.js
+++ db0/nodejsdb/express-couch/public/client.js
@@ -2,18 +2,20 @@ console.log('Client-side code running');
 
 const button = document.getElementById('myButton');
 button.addEventListener('click', function(e) {
-  console.log('button was clicked');
-
-  fetch('/clicked', {method: 'POST'})
-    .then(function(response) {
-      if(response.ok) {
-        console.log('Click was recorded');
-        return;
-      }
-      throw new Error('Request failed.');
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+    console.log('button was clicked');
+    
+    fetch('/clicked', {method: 'POST'})
+        .then(function(response) {
+            console.log("Click was recorded");
+            console.log("Click was recorded: response.rescnt ->" + response.get("rescnt") + "<-");
+            if(response.ok) {
+                console.log("Click was recorded: response ->" + response.rescnt + "<-");
+                return;
+            }
+            throw new Error('Request failed.');
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
 });
 
