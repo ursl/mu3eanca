e72139729614353d35aef6a472a5d138d6753e42
Urs Langenegger <urslangenegger@gmail.com>
2023-06-12 14:54:04 +0200

    migrated from string to payload

---
+++ db0/cdb/payload.cc
@@ -0,0 +1,6 @@
+#include "payload.hh"
+
+// ----------------------------------------------------------------------
+payload::payload() : fLength(0), fComment("now what?"), fBLOB(std::string("empty payload")) {
+
+};
