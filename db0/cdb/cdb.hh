33381d4589d6824318012a9b8e6d099d0ee363f0
Urs Langenegger <urslangenegger@gmail.com>
2023-06-15 18:02:42 +0200

    changes: add pixelalignment_ and createClass(string)

--- db0/cdb/cdb.hh
+++ db0/cdb/cdb.hh
@@ -23,7 +23,7 @@ public:
 	// -- access to metadata (in DB)
   std::string                                  getGlobalTag() {return fGT;}
   virtual std::vector<std::string>             getGlobalTags() {return fGlobalTags;}
-  virtual std::vector<std::string>             getTags(std::string gt) {return fTags;}
+  virtual std::vector<std::string>             getTags() {return fTags;}
   virtual std::vector<int>                     getIOVs(std::string t) {return fIOVs[t];}
 
   // -- access to payloads and IOVs
