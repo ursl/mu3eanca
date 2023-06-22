0675dc5b605ee88a0a669668ce66e648e7a84dcf
Urs Langenegger <urslangenegger@gmail.com>
2023-06-22 11:42:07 +0200

    update to cover all fields

--- qnd/dumpSensorsToCSV.C
+++ qnd/dumpSensorsToCSV.C
@@ -2,11 +2,32 @@
 void csv(string filename = "sensors.csv") {
   TTree *t = (TTree*)gFile->Get("alignment/sensors");
   double vx, vy, vz;
+  double rowx, rowy, rowz;
+  double colx, coly, colz;
   UInt_t sensor;
+  Int_t  nrow, ncol;
+  double width, length, thickness, pixelSize;
+  
+  t->SetBranchAddress("sensor", &sensor);
+
   t->SetBranchAddress("vx", &vx);
   t->SetBranchAddress("vy", &vy);
   t->SetBranchAddress("vz", &vz);
-  t->SetBranchAddress("sensor", &sensor);
+
+  t->SetBranchAddress("rowx", &rowx);
+  t->SetBranchAddress("rowy", &rowy);
+  t->SetBranchAddress("rowz", &rowz);
+
+  t->SetBranchAddress("colx", &colx);
+  t->SetBranchAddress("coly", &coly);
+  t->SetBranchAddress("colz", &colz);
+
+  t->SetBranchAddress("nrow", &nrow);
+  t->SetBranchAddress("ncol", &ncol);
+  t->SetBranchAddress("width", &width);
+  t->SetBranchAddress("length", &length);
+  t->SetBranchAddress("thickness", &thickness);
+  t->SetBranchAddress("pixelSize", &pixelSize);
 
   ofstream ONS;
   ONS.open(filename);
@@ -14,15 +35,28 @@ void csv(string filename = "sensors.csv") {
   for (unsigned int i = 0; i < t->GetEntries(); ++i) {
     t->GetEntry(i);
     cout << "sensor = " << sensor
-         << " vx/vy/vz = " << vx << "/" << vy << "/" << vz
+         << " v x/y/z = " << vx << "/" << vy << "/" << vz
+         << " row x/y/z = " << rowx << "/" << rowy << "/" << rowz
+         << " col x/y/z = " << colx << "/" << coly << "/" << colz
+         << " rest: " << nrow << "/" << ncol << "/" << width
+         << "/" << length << "/" << thickness << "/" << pixelSize
          << endl;
     ONS << sensor << ","
         << vx << ","
         << vy << ","
         << vz << ","
-        << "0.0,"
-        << "0.0,"
-        << "0.0,"
+        << rowx << ","
+        << rowy << ","
+        << rowz << ","
+        << colx << ","
+        << coly << ","
+        << colz << ","
+        << nrow << ","
+        << ncol << ","
+        << width << ","
+        << length << ","
+        << thickness << ","
+        << pixelSize
         << endl;
   }
 
