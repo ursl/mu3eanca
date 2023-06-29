edf56e4a9855dfa5f94ddccd1c013af7d0375f25
Urs Langenegger <urslangenegger@gmail.com>
2023-06-29 08:43:36 +0200

    this now works in two ways

--- qnd/dumpSensorsToBLOB.C
+++ qnd/dumpSensorsToBLOB.C
@@ -2,30 +2,6 @@
 #include <array>
 
 // ----------------------------------------------------------------------
-//Int_t blob2Int(char data[8]) {
-//Int_t a(0);
-//memcpy(&a, data, sizeof a);
-//return a;
-//}
-
-
-// ----------------------------------------------------------------------
-//UInt_t blob2UInt(char data[8]) {
-//UInt_t a(0);
-//memcpy(&a, data, sizeof a);
-//return a;
-//}
-
-
-// ----------------------------------------------------------------------
-//double blob2Double(char data[8]) {
-// double a(0.0);
-//  memcpy(&a, data, sizeof a);
-//  return a;
-//}
-
-
-// ----------------------------------------------------------------------
 Int_t blob2Int(std::array<char,8> v) {
   char data[8] = {0}; 
   for (int i = 0; i < 8; ++i) data[i] = v[i];
@@ -106,6 +82,7 @@ void writeBlob(string filename = "sensors.bin") {
   double colx, coly, colz;
   UInt_t sensor;
   Int_t  nrow, ncol;
+  long unsigned int header(0xdeadface);
   double width, length, thickness, pixelSize;
   
   t->SetBranchAddress("sensor", &sensor);
@@ -131,7 +108,9 @@ void writeBlob(string filename = "sensors.bin") {
 
   ofstream ONS;
   ONS.open(filename);
-
+  //  printArray(ONS, uint2Blob(header));
+  ONS << dumpArray(uint2Blob(header));
+  
   char data[8], data1[8], data2[8]; 
   for (unsigned int i = 0; i < t->GetEntries(); ++i) {
     t->GetEntry(i);
@@ -142,40 +121,44 @@ void writeBlob(string filename = "sensors.bin") {
                 << " rest: " << nrow << "/" << ncol << "/" << width
                 << "/" << length << "/" << thickness << "/" << pixelSize
                 << endl;
-    printArray(ONS, uint2Blob(sensor));
-    printArray(ONS, double2Blob(vx));
-    printArray(ONS, double2Blob(vy));
-    printArray(ONS, double2Blob(vz));
-    printArray(ONS, double2Blob(rowx));
-    printArray(ONS, double2Blob(rowy));
-    printArray(ONS, double2Blob(rowz));
-    printArray(ONS, double2Blob(colx));
-    printArray(ONS, double2Blob(coly));
-    printArray(ONS, double2Blob(colz));
-    printArray(ONS, int2Blob(nrow));
-    printArray(ONS, int2Blob(ncol));
-    printArray(ONS, double2Blob(width));
-    printArray(ONS, double2Blob(length));
-    printArray(ONS, double2Blob(thickness));
-    printArray(ONS, double2Blob(pixelSize));
+    if (0) {
+      printArray(ONS, uint2Blob(sensor));
+      printArray(ONS, double2Blob(vx));
+      printArray(ONS, double2Blob(vy));
+      printArray(ONS, double2Blob(vz));
+      printArray(ONS, double2Blob(rowx));
+      printArray(ONS, double2Blob(rowy));
+      printArray(ONS, double2Blob(rowz));
+      printArray(ONS, double2Blob(colx));
+      printArray(ONS, double2Blob(coly));
+      printArray(ONS, double2Blob(colz));
+      printArray(ONS, int2Blob(nrow));
+      printArray(ONS, int2Blob(ncol));
+      printArray(ONS, double2Blob(width));
+      printArray(ONS, double2Blob(length));
+      printArray(ONS, double2Blob(thickness));
+      printArray(ONS, double2Blob(pixelSize));
+    }
+    
+    if (1) {
+      ONS << dumpArray(uint2Blob(sensor)) 
+          << dumpArray(double2Blob(vx)) 
+          << dumpArray(double2Blob(vy)) 
+          << dumpArray(double2Blob(vz)) 
+          << dumpArray(double2Blob(rowx))
+          << dumpArray(double2Blob(rowy))
+          << dumpArray(double2Blob(rowz))
+          << dumpArray(double2Blob(colx))
+          << dumpArray(double2Blob(coly))
+          << dumpArray(double2Blob(colz))
+          << dumpArray(int2Blob(nrow))
+          << dumpArray(int2Blob(ncol))
+          << dumpArray(double2Blob(width))
+          << dumpArray(double2Blob(length))
+          << dumpArray(double2Blob(thickness))
+          << dumpArray(double2Blob(pixelSize));
+    }
 
-    if (0)    ONS << dumpArray(uint2Blob(sensor)) 
-                  << dumpArray(double2Blob(vx)) 
-                  << dumpArray(double2Blob(vy)) 
-                  << dumpArray(double2Blob(vz)) 
-                  << dumpArray(double2Blob(rowx))
-                  << dumpArray(double2Blob(rowy))
-                  << dumpArray(double2Blob(rowz))
-                  << dumpArray(double2Blob(colx))
-                  << dumpArray(double2Blob(coly))
-                  << dumpArray(double2Blob(colz))
-                  << dumpArray(int2Blob(nrow))
-                  << dumpArray(int2Blob(ncol))
-                  << dumpArray(double2Blob(width))
-                  << dumpArray(double2Blob(length))
-                  << dumpArray(double2Blob(thickness))
-                  << dumpArray(double2Blob(pixelSize))
-                  << endl;
   }
   ONS.close();
 }
@@ -200,14 +183,17 @@ void readBlob(string filename = "sensors.bin") {
   UInt_t sensor;
   Int_t  nrow, ncol;
   double width, length, thickness, pixelSize;
+  long unsigned int header;
   
   ifstream INS;
   INS.open(filename);
 
   vector<char> buffer(std::istreambuf_iterator<char>(INS), {});
-  
-  std::array<char,8> v;
   std::vector<char>::iterator ibuffer = buffer.begin();
+
+  header = blob2UInt(getData(ibuffer)); ibuffer += 8;
+  cout << "header: " << hex << header << dec << endl;
+
   while (ibuffer != buffer.end()) {
     sensor = blob2UInt(getData(ibuffer)); ibuffer += 8;
     vx = blob2Double(getData(ibuffer)); ibuffer += 8;
