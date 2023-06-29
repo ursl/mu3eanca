f7b0c19758af985c06f0b0e3fe26629d061f20f8
Urs Langenegger <urslangenegger@gmail.com>
2023-06-29 09:09:14 +0200

    add BLOB utilities

--- db0/cdb1/cdbUtil.cc
+++ db0/cdb1/cdbUtil.cc
@@ -4,10 +4,85 @@
 #include <sstream>
 #include <algorithm>
 #include <unistd.h>
+#include <cstring>
 
 #include "cdbUtil.hh"
 
 using namespace std;
+
+
+// ----------------------------------------------------------------------
+int blob2Int(std::array<char,8> v) {
+  char data[8] = {0}; 
+  for (int i = 0; i < 8; ++i) data[i] = v[i];
+  int a(0);
+  memcpy(&a, data, sizeof a);
+  return a;
+}
+
+
+// ----------------------------------------------------------------------
+unsigned int blob2UnsignedInt(std::array<char,8> v) {
+  char data[8] = {0}; 
+  for (int i = 0; i < 8; ++i) data[i] = v[i];
+  unsigned int a(0);
+  memcpy(&a, data, sizeof a);
+  return a;
+}
+
+
+// ----------------------------------------------------------------------
+double blob2Double(std::array<char,8> v) {
+  char data[8] = {0}; 
+  for (int i = 0; i < 8; ++i) data[i] = v[i];
+  double a(0.0);
+  memcpy(&a, data, sizeof a);
+  return a;
+}
+
+
+// ----------------------------------------------------------------------
+std::array<char,8> int2Blob(int a) {
+  char data[8] = {0}; 
+  memcpy(data, &a, sizeof a);
+  array<char,8> v; 
+  for (int i = 0; i < 8; ++i) v[i] = data[i]; 
+  return v;
+}
+
+// ----------------------------------------------------------------------
+std::array<char,8> uint2Blob(unsigned int a) {
+  char data[8] = {0}; 
+  memcpy(data, &a, sizeof a);
+  array<char,8> v; 
+  for (int i = 0; i < 8; ++i) v[i] = data[i]; 
+  return v;
+}
+
+
+// ----------------------------------------------------------------------
+std::array<char,8> double2Blob(double a) {
+  char data[8] = {0}; 
+  memcpy(data, &a, sizeof a);
+  array<char,8> v; 
+  for (int i = 0; i < 8; ++i) v[i] = data[i]; 
+  return v;
+}
+
+
+// ----------------------------------------------------------------------
+string dumpArray(std::array<char,8> v) {
+  stringstream sstr;
+  for (auto it: v) sstr << it; 
+  return sstr.str();
+}
+
+
+// ----------------------------------------------------------------------
+void printArray(ofstream &OS, std::array<char,8> v) {
+  for (auto it: v) OS << it; 
+}
+
 
 // ----------------------------------------------------------------------
 void replaceAll(string &str, const string &from, const string &to) {
