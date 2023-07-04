7d335641bf13d7cc89c6535404e0c7bd64c9fb92
Urs Langenegger <urslangenegger@gmail.com>
2023-07-04 08:51:48 +0200

    add tile alignment

---
+++ db0/cdb1/calTileAlignment.cc
@@ -0,0 +1,80 @@
+#include "calTileAlignment.hh"
+
+#include "cdbUtil.hh"
+
+#include <iostream>
+#include <sstream>
+
+
+using namespace std;
+
+// ----------------------------------------------------------------------
+calTileAlignment::calTileAlignment(cdbAbs *db) : calAbs(db) {
+}
+
+
+// ----------------------------------------------------------------------
+bool calTileAlignment::getNextID(uint32_t &ID) {
+  if (fMapConstantsIt == fMapConstants.end()) {
+    // -- reset
+    ID = 999999;
+    fMapConstantsIt = fMapConstants.begin();
+    return false;
+  } else {
+    ID = fMapConstantsIt->first;
+    fMapConstantsIt++;
+  }
+  return true;
+}
+
+
+// ----------------------------------------------------------------------
+calTileAlignment::calTileAlignment(cdbAbs *db, string tag) : calAbs(db, tag) {
+	cout << "calTileAlignment created and registered with tag ->" << fTag << "<-" 
+			 << endl;
+}
+
+
+// ----------------------------------------------------------------------
+calTileAlignment::~calTileAlignment() {
+  cout << "this is the end of calTileAlignment with tag ->" << fTag << "<-" << endl;
+}
+
+
+// ----------------------------------------------------------------------
+void calTileAlignment::calculate(string hash) {
+  cout << "calTileAlignment::calculate() with "
+       << "fHash ->" << hash << "<-"
+       << endl;
+  fMapConstants.clear();
+  string spl = fTagIOVPayloadMap[hash].fBLOB;
+
+  std::vector<char> buffer(spl.begin(), spl.end());
+  std::vector<char>::iterator ibuffer = buffer.begin();
+  
+  long unsigned int header = blob2UnsignedInt(getData(ibuffer)); 
+  cout << "calTileAlignment header: " << hex << header << dec << endl;
+
+  while (ibuffer != buffer.end()) {
+    constants a; 
+    a.sensor = blob2Int(getData(ibuffer));
+    a.id = blob2UnsignedInt(getData(ibuffer));
+    a.posx = blob2Double(getData(ibuffer));
+    a.posy = blob2Double(getData(ibuffer));
+    a.posz = blob2Double(getData(ibuffer));
+    a.dirx = blob2Double(getData(ibuffer));
+    a.diry = blob2Double(getData(ibuffer));
+    a.dirz = blob2Double(getData(ibuffer));
+
+    cout << "sensor = " << a.sensor << " id = " << a.id
+         << " pos = " << a.posx << "/" << a.posy << "/" << a.posz
+         << " dir = " << a.dirx << "/" << a.diry << "/" << a.dirz
+         << endl;
+    
+    fMapConstants.insert(make_pair(a.id, a));
+  }
+
+  // -- set iterator over all constants to the start of the map
+  fMapConstantsIt = fMapConstants.begin();
+}
+
---
+++ db0/cdb1/calTileAlignment.hh
@@ -0,0 +1,52 @@
+#ifndef CALTILELALIGNMENT_h
+#define CALTILEALIGNMENT_h
+
+#include "calAbs.hh"
+
+#include <string>
+#include <map>
+
+// ----------------------------------------------------------------------
+// tile alignment class 
+// ----------------------------------------------------------------------
+class calTileAlignment : public calAbs {
+public:
+  
+  calTileAlignment() = default;
+  calTileAlignment(cdbAbs *db);
+  calTileAlignment(cdbAbs *db, std::string tag);
+  ~calTileAlignment();
+
+  // -- direct accessors
+  uint32_t id(uint32_t id) {return fMapConstants[id].id;}
+  int      sensor(uint32_t id) {return fMapConstants[id].sensor;}
+  double   posx(uint32_t id) {return fMapConstants[id].posx;}
+  double   posy(uint32_t id) {return fMapConstants[id].posy;}
+  double   posz(uint32_t id) {return fMapConstants[id].posz;}
+  double   dirx(uint32_t id) {return fMapConstants[id].dirx;}
+  double   diry(uint32_t id) {return fMapConstants[id].diry;}
+  double   dirz(uint32_t id) {return fMapConstants[id].dirz;}
+
+  std::string getName() override {return fTileAlignmentTag;}
+  void        calculate(std::string hash) override;
+
+  bool        getNextID(uint32_t &ID);
+  
+private:
+  std::string fTileAlignmentTag{"tilealignment_"};
+
+  // -- local and private
+  struct constants {
+    int sensor;
+    uint32_t id; 
+    double posx, posy, posz;
+    double dirx, diry, dirz;
+  };
+
+  std::map<uint32_t, constants> fMapConstants;
+  std::map<uint32_t, constants>::iterator fMapConstantsIt{fMapConstants.end()};
+  
+  double *fpvx;
+};
+
+#endif
