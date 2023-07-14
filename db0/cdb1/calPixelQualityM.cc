86bf8bac6c9d73574efc9696c7c813b54630268d
Urs Langenegger <urslangenegger@gmail.com>
2023-07-14 17:18:44 +0200

    add map implementation (using int idx = 250*icol + irow)

---
+++ db0/cdb1/calPixelQualityM.cc
@@ -0,0 +1,87 @@
+#include "calPixelQualityM.hh"
+
+#include "cdbUtil.hh"
+
+#include <iostream>
+#include <sstream>
+
+
+using namespace std;
+
+// ----------------------------------------------------------------------
+calPixelQualityM::calPixelQualityM(cdbAbs *db) : calAbs(db) {
+}
+
+
+// ----------------------------------------------------------------------
+bool calPixelQualityM::getNextID(uint32_t &ID) {
+  if (fMapConstantsIt == fMapConstants.end()) {
+    // -- reset
+    ID = 999999;
+    fMapConstantsIt = fMapConstants.begin();
+    return false;
+  } else {
+    ID = fMapConstantsIt->first;
+    fMapConstantsIt++;
+  }
+  return true;
+}
+
+
+// ----------------------------------------------------------------------
+calPixelQualityM::calPixelQualityM(cdbAbs *db, string tag) : calAbs(db, tag) {
+	cout << "calPixelQualityM created and registered with tag ->" << fTag << "<-" 
+			 << endl;
+}
+
+
+// ----------------------------------------------------------------------
+calPixelQualityM::~calPixelQualityM() {
+  for (auto it: fMapConstants) it.second.mpixel.clear();    
+  fMapConstants.clear();
+  cout << "this is the end of calPixelQualityM with tag ->" << fTag << "<-" << endl;
+}
+
+
+// ----------------------------------------------------------------------
+void calPixelQualityM::calculate(string hash) {
+  cout << "calPixelQualityM::calculate() with "
+       << "fHash ->" << hash << "<-"
+       << endl;
+  fMapConstants.clear();
+  string spl = fTagIOVPayloadMap[hash].fBLOB;
+
+  std::vector<char> buffer(spl.begin(), spl.end());
+  std::vector<char>::iterator ibuffer = buffer.begin();
+  
+  long unsigned int header = blob2UnsignedInt(getData(ibuffer)); 
+  cout << "calPixelQualityM header: " << hex << header << dec << endl;
+  
+  int npix(0);
+  while (ibuffer != buffer.end()) {
+    constants a; 
+    a.id = blob2UnsignedInt(getData(ibuffer));
+    // -- get number of pixel entries
+    npix = blob2Int(getData(ibuffer));
+    
+    for (unsigned int i = 0; i < npix; ++i) {
+      int icol            = blob2Int(getData(ibuffer));
+      int irow            = blob2Int(getData(ibuffer));
+      unsigned int iqual = blob2UnsignedInt(getData(ibuffer));
+      int idx = icol*250 + irow;
+      a.mpixel.insert(make_pair(idx, static_cast<char>(iqual)));
+    }
+    // cout << "inserting " << a.id << " with size = " << sizeof(a) << endl;
+    fMapConstants.insert(make_pair(a.id, a));
+  }
+
+  // -- set iterator over all constants to the start of the map
+  fMapConstantsIt = fMapConstants.begin();
+}
+
+// ----------------------------------------------------------------------
+int calPixelQualityM::getStatus(unsigned int chipid, int icol, int irow) {
+  constants a = fMapConstants[chipid];
+  int idx = icol*250 + irow;
+  return static_cast<int>(a.mpixel[idx]);
+}
---
+++ db0/cdb1/calPixelQualityM.hh
@@ -0,0 +1,46 @@
+#ifndef CALPIXELQUALITYM_h
+#define CALPIXELQUALITYM_h
+
+#include "calAbs.hh"
+
+#include <string>
+#include <map>
+
+
+// ----------------------------------------------------------------------
+// pixel quality class 
+// ----------------------------------------------------------------------
+class calPixelQualityM : public calAbs {
+public:
+  
+  calPixelQualityM() = default;
+  calPixelQualityM(cdbAbs *db);
+  calPixelQualityM(cdbAbs *db, std::string tag);
+  ~calPixelQualityM();
+
+  // -- direct accessors
+  uint32_t id(uint32_t id) {return fMapConstants[id].id;}
+
+  std::string getName() override {return fPixelQualityTag;}
+  void        calculate(std::string hash) override;
+
+  int         getStatus(unsigned int chipid, int icol, int irow)  override;
+
+  bool        getNextID(uint32_t &ID);
+  void        printPixelQuality(unsigned int chipid, int minimumStatus = 0);
+  
+private:
+  std::string fPixelQualityTag{"pixelqualitym_"};
+
+ 
+  // -- local and private
+  struct constants {
+    uint32_t id; 
+    std::map<int, char> mpixel;
+  };
+
+  std::map<uint32_t, constants> fMapConstants;
+  std::map<uint32_t, constants>::iterator fMapConstantsIt{fMapConstants.end()};
+};
+
+#endif
