d68acaa9f7907757bb8a8486ca037b2bb2e3de6c
Urs Langenegger <urslangenegger@gmail.com>
2023-08-11 15:26:23 +0200

    snapshot

--- db0/cdb1/mdc2023FillDB.cc
+++ db0/cdb1/mdc2023FillDB.cc
@@ -27,10 +27,12 @@ using bsoncxx::builder::stream::open_document;
 using namespace std;
 
 // ----------------------------------------------------------------------
-// fillDB [-json]
+// mdc2023FillDB [-j JSONDIR]
 // --------------
 //
-// -json  ONLY the JSON file-based DB is written, but not the mongo DB (useful for merlin)
+// -j JSONDIR
+//
+// This creates ONLY payloads/tags/iovs/gt required for MDC2023
 //
 // requires ../ascii/sensors-*.csv
 //
@@ -41,28 +43,30 @@ using namespace std;
 // to contain some numerical changes to see changes in the payload
 //
 // Usage:
-// bin/fillDB
+// merlin> bin/mdc2023FillDB -d ~/data/mdc2023/json/
+// moor>   bin/mdc2023FillDB -d ~/data/mu3e/mdc2023/json
 // 
 // ----------------------------------------------------------------------
 
 // ----------------------------------------------------------------------=
 int main(int argc, char* argv[]) {
 
-
   // -- command line arguments
-  bool json(0);
-  string password("fixme");
+  string jsondir("");
   for (int i = 0; i < argc; i++){
-    if (!strcmp(argv[i], "-json"))  {json = true;}
-    if (!strcmp(argv[i], "-pw"))  {password = argv[++i];}
+    if (!strcmp(argv[i], "-j"))  {jsondir = argv[++i];;}
   }
 
   // -- check whether directories for JSONs already exist
-  DIR *folder = opendir("json/payloads");
+  DIR *folder = opendir(string(jsondir + "/payloads").c_str());
   if (folder == NULL) {
     system("mkdir -p json/payloads");
     system("mkdir -p json/globaltags");
     system("mkdir -p json/iovs");
+
+    system(string("mkdir -p " + jsondir + "/payloads").c_str());
+    system(string("mkdir -p " + jsondir + "/globaltags").c_str());
+    system(string("mkdir -p " + jsondir + "/iovs").c_str());
   }
   closedir(folder);
   
@@ -84,42 +88,18 @@ int main(int argc, char* argv[]) {
 	mongocxx::collection globaltags;
 	mongocxx::collection iovs;
 	mongocxx::collection payloads;
-  if (!json) db = client["mu3e"];
     
 	// ----------------------------------------------------------------------
 	// -- global tags
 	// ----------------------------------------------------------------------
-  if (!json) {
-    globaltags = db["globaltags"];
-    globaltags.drop();
-  }
-
 	map<string, vector<string>> iniGlobalTags = {
-		{"dt23intrun",
-     {"pixel_dt23intrun",
-      "pixelalignment_dt23intrun",
-      "fibrealignment_dt23intrun",
-      "tilealignment_Nada"}}, 
-		{"dt23prompt",
-     {"pixel_v0",
-      "pixelalignment_v0",
-      "fibrealignment_v0",
-      "tilealignment_A"}}, 
-		{"mcideal",
-     {"pixel_mcideal",
-      "pixelalignment_mcideal",
-      "fibrealignment_mcideal",
-      "mppcalignment_mcideal",
-      "tilealignment_mcideal"}}, 
-		{"mc23intrun",
-     {"pixel_mc23intrun",
-      "pixelalignment_mc23intrun",
-      "fibrealignment_mc23intrun",
-      "tilealignment_Nada"}}
-	};
+		{"mdc2023",
+     {"pixelalignment_mdc2023", "pixelquality_mdc2023", "pixelcablingmap_mdc2023"}
+    }
+  };
 	
-	string jdir  = "json/globaltags";
-
+	string jdir  = jsondir + "/globaltags";
+  
   for (auto igt : iniGlobalTags) {
 		auto array_builder = bsoncxx::builder::basic::array{};
 		for (auto it : igt.second) array_builder.append(it);
@@ -127,11 +107,6 @@ int main(int argc, char* argv[]) {
 			<< "gt" << igt.first
 			<< "tags" << array_builder
 			<< finalize; 
-
-		if (!json) {
-      bsoncxx::stdx::optional<mongocxx::result::insert_one> result = globaltags.insert_one(doc_value.view());
-      if (!result)  cout << "Failed to insert" << endl;
-    }
     
     // -- JSON
     JS.open(jdir + "/" + igt.first);
@@ -140,7 +115,6 @@ int main(int argc, char* argv[]) {
     }
     JS << bsoncxx::to_json(doc_value.view()) << endl;
     JS.close();
-
   }
 
 	// ----------------------------------------------------------------------
@@ -152,30 +126,10 @@ int main(int argc, char* argv[]) {
   }
   
 	map<string, vector<int>> iniIovs = {
-		{"pixelalignment_dt23intrun", {1,200}},
-		{"pixelalignment_v0", {1,200}},
-		{"pixelalignment_mc23intrun", {200}}, 
-		{"pixelalignment_mcideal", {1,300}},
-    // 
-		{"pixel_dt23intrun", {1,10,20,30,100,200}},
-		{"pixel_v0", {202,210,900}}, 
-		{"pixel_mcideal", {1}},
-		{"pixel_mc23intrun", {200}}, 
-    //
-		{"tilealignment_Nada", {1}}, 
-		{"tilealignment_A", {202,300,700}},
-		{"tilealignment_mcideal", {1,400}},
-		{"tilealignment_mc23ideal", {1}},
-    //
-		{"fibrealignment_dt23intrun", {1,200}},
-		{"fibrealignment_v11", {202,400,800}},
-		{"fibrealignment_mcideal", {1,300}},
-		{"fibrealignment_mc23intrun", {150}},
-    //
-    {"mppcalignment_mcideal", {1,300}},
+		{"pixelalignment_mdc2023", {1}}, {"pixelquality_mdc2023", {1}}, {"pixelcablingmap_mdc2023", {1}}
 	};
 
-	jdir  = "json/iovs";
+	jdir  = jsondir + "/iovs";
 
 	for (auto iiov : iniIovs) {
 		auto array_builder = bsoncxx::builder::basic::array{};
@@ -185,11 +139,6 @@ int main(int argc, char* argv[]) {
 			<< "iovs" << array_builder
 			<< finalize; 
 		
-    if (!json) {
-      bsoncxx::stdx::optional<mongocxx::result::insert_one> result = iovs.insert_one(doc_value.view());
-      if (!result)  cout << "Failed to insert into iovs" << endl;
-		}
-    
     // -- JSON
     JS.open(jdir + "/" + iiov.first);
     if (JS.fail()) {
@@ -203,65 +152,18 @@ int main(int argc, char* argv[]) {
 	// ----------------------------------------------------------------------
 	// -- payloads
 	// ----------------------------------------------------------------------
-  if (!json) {
-    payloads = db["payloads"];
-    payloads.drop();
-  }
-
-	jdir = "json/payloads";
-
-	for (auto iiov: iniIovs) {
-    // -- do the pixelalignment separately below
-    if (string::npos != iiov.first.find("pixelalignment")) continue;
-		for (auto it : iiov.second) {
-			stringstream sh; 
-			sh << "tag_" << iiov.first;
-			sh << "_iov_" << it;
-			stringstream sp;
-			sp << "payload " << iiov.first << " for iov " << it;
-
-			cout << sh.str() << "-> " << sp.str() << endl;
-			
-			bsoncxx::document::value doc_value = builder
-				<< "hash" << sh.str()
-				<< "comment" << "testing"
-				<< "BLOB" << sp.str()
-				<< finalize; 
-
-      if (!json) {
-        bsoncxx::stdx::optional<mongocxx::result::insert_one> result = payloads.insert_one(doc_value.view());
-        if (!result)  cout << "Failed to insert into iovs" << endl;
-      }
-      
-      // -- JSON
-      JS.open(jdir + "/" + sh.str());
-      if (JS.fail()) {
-        cout << "Error failed to open " << jdir << "/" << iiov.first << endl;
-      }
-      JS << bsoncxx::to_json(doc_value.view()) << endl;
-      JS.close();
-    }
-	}
-  
+	jdir = jsondir + "/payloads";
 
   // -- pixelalignment
 	for (auto iiov: iniIovs) {
     if (string::npos == iiov.first.find("pixelalignment")) continue;
-    int cnt(0); 
 		for (auto it : iiov.second) {
 			stringstream sh; 
 			sh << "tag_" << iiov.first;
 			sh << "_iov_" << it;
-
+      
       std::ifstream file;
-      // -- Note: 779 should be the same as the normal one
-      if (string::npos != iiov.first.find("intrun")) {
-        if (1 == cnt%2) file.open("../ascii/sensors-intrun.bin");
-        if (0 == cnt%2) file.open("../ascii/sensors-intrun-1.bin");
-      } else {
-        if (1 == cnt%2) file.open("../ascii/sensors-full.bin");
-        if (0 == cnt%2) file.open("../ascii/sensors-full-1.bin");
-      }
+      file.open("../ascii/sensors-mdc2023.bin");
       vector<char> buffer(std::istreambuf_iterator<char>(file), {});
       string sblob("");
       for (unsigned int i = 0; i < buffer.size(); ++i) sblob.push_back(buffer[i]);
@@ -276,11 +178,6 @@ int main(int argc, char* argv[]) {
 				<< "BLOB" << base64_encode(sblob)
 				<< finalize; 
 			
-      if (!json) {
-        bsoncxx::stdx::optional<mongocxx::result::insert_one> result = payloads.insert_one(doc_value.view());
-        if (!result)  cout << "Failed to insert into iovs" << endl;
-      }
-      
       // -- JSON
       JS.open(jdir + "/" + sh.str());
       if (JS.fail()) {
@@ -288,24 +185,19 @@ int main(int argc, char* argv[]) {
       }
       JS << bsoncxx::to_json(doc_value.view()) << endl;
       JS.close();
-      ++cnt;
     }
 	}
 
-
-  // -- fibres
+  // -- pixelquality
 	for (auto iiov: iniIovs) {
-    if (string::npos == iiov.first.find("fibrealignment")) continue;
-    int cnt(0); 
+    if (string::npos == iiov.first.find("pixelquality")) continue;
 		for (auto it : iiov.second) {
 			stringstream sh; 
 			sh << "tag_" << iiov.first;
 			sh << "_iov_" << it;
 
       std::ifstream file;
-      // -- Note: 779 should be the same as the normal one
-      if (0 == cnt%2) file.open("../ascii/fibres-full-1.bin");
-      if (1 == cnt%2) file.open("../ascii/fibres-full.bin");
+      file.open("../ascii/sensors-intrun-1.bin");
       vector<char> buffer(std::istreambuf_iterator<char>(file), {});
       string sblob("");
       for (unsigned int i = 0; i < buffer.size(); ++i) sblob.push_back(buffer[i]);
@@ -320,11 +212,6 @@ int main(int argc, char* argv[]) {
 				<< "BLOB" << base64_encode(sblob)
 				<< finalize; 
 			
-      if (!json) {
-        bsoncxx::stdx::optional<mongocxx::result::insert_one> result = payloads.insert_one(doc_value.view());
-        if (!result)  cout << "Failed to insert into iovs" << endl;
-      }
-      
       // -- JSON
       JS.open(jdir + "/" + sh.str());
       if (JS.fail()) {
@@ -332,96 +219,8 @@ int main(int argc, char* argv[]) {
       }
       JS << bsoncxx::to_json(doc_value.view()) << endl;
       JS.close();
-      ++cnt;
     }
 	}
 
-  // -- mppcs
-	for (auto iiov: iniIovs) {
-    if (string::npos == iiov.first.find("mppcalignment")) continue;
-    int cnt(0); 
-		for (auto it : iiov.second) {
-			stringstream sh; 
-			sh << "tag_" << iiov.first;
-			sh << "_iov_" << it;
-
-      std::ifstream file;
-      // -- Note: 779 should be the same as the normal one
-      if (0 == cnt%2) file.open("../ascii/mppcs-full-1.bin");
-      if (1 == cnt%2) file.open("../ascii/mppcs-full.bin");
-      vector<char> buffer(std::istreambuf_iterator<char>(file), {});
-      string sblob("");
-      for (unsigned int i = 0; i < buffer.size(); ++i) sblob.push_back(buffer[i]);
-      std::vector<char>::iterator ibuffer = buffer.begin();
-      long unsigned int header = blob2UnsignedInt(getData(ibuffer)); 
-      cout << "header: " << hex
-           << header << endl;
-      
-			bsoncxx::document::value doc_value = builder
-				<< "hash" << sh.str()
-				<< "comment" << "testing"
-				<< "BLOB" << base64_encode(sblob)
-				<< finalize; 
-			
-      if (!json) {
-        bsoncxx::stdx::optional<mongocxx::result::insert_one> result = payloads.insert_one(doc_value.view());
-        if (!result)  cout << "Failed to insert into iovs" << endl;
-      }
-      
-      // -- JSON
-      JS.open(jdir + "/" + sh.str());
-      if (JS.fail()) {
-        cout << "Error failed to open " << jdir << "/" << iiov.first << endl;
-      }
-      JS << bsoncxx::to_json(doc_value.view()) << endl;
-      JS.close();
-      ++cnt;
-    }
-	}
-
-  // -- tiles
-	for (auto iiov: iniIovs) {
-    if (string::npos == iiov.first.find("tilealignment")) continue;
-    int cnt(0); 
-		for (auto it : iiov.second) {
-			stringstream sh; 
-			sh << "tag_" << iiov.first;
-			sh << "_iov_" << it;
-
-      std::ifstream file;
-      // -- Note: 779 should be the same as the normal one
-      if (0 == cnt%2) file.open("../ascii/tiles-full-1.bin");
-      if (1 == cnt%2) file.open("../ascii/tiles-full.bin");
-      vector<char> buffer(std::istreambuf_iterator<char>(file), {});
-      string sblob("");
-      for (unsigned int i = 0; i < buffer.size(); ++i) sblob.push_back(buffer[i]);
-      std::vector<char>::iterator ibuffer = buffer.begin();
-      long unsigned int header = blob2UnsignedInt(getData(ibuffer)); 
-      cout << "header: " << hex
-           << header << endl;
-      
-			bsoncxx::document::value doc_value = builder
-				<< "hash" << sh.str()
-				<< "comment" << "testing"
-				<< "BLOB" << base64_encode(sblob)
-				<< finalize; 
-			
-      if (!json) {
-        bsoncxx::stdx::optional<mongocxx::result::insert_one> result = payloads.insert_one(doc_value.view());
-        if (!result)  cout << "Failed to insert into iovs" << endl;
-      }
-      
-      // -- JSON
-      JS.open(jdir + "/" + sh.str());
-      if (JS.fail()) {
-        cout << "Error failed to open " << jdir << "/" << iiov.first << endl;
-      }
-      JS << bsoncxx::to_json(doc_value.view()) << endl;
-      JS.close();
-      ++cnt;
-    }
-	}
-
-  
 	return 0;
 }
