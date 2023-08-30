888752b955e09e15a7be16ec4971f5064200f4a7
Urs Langenegger <urslangenegger@gmail.com>
2023-08-30 16:16:58 +0200

    re-shuffle; create all payloads with input; setup for mcideal and intrun

--- db0/cdb1/cdbInitDB.cc
+++ db0/cdb1/cdbInitDB.cc
@@ -40,8 +40,13 @@ using namespace std;
 // cdbInitDB [-j JSONDIR] [-m MODE]
 // ---------
 //
+// initialize the JSON filesystem-based CDB for several starting points
+// "intrun"   the integration run detector of 2022
+// "mcideal"  complete detector as contained in mu3e sim files alignment/* trees
+//
+// 
 // -j JSONDIR  output directory with subdirectories globaltags, tags, payloads
-// -m MODE     "mdc2023", "mcideal"
+// -m MODE     "intrun", "mcideal"
 //
 // requires ../ascii/*.csv
 //
@@ -59,6 +64,7 @@ int main(int argc, char* argv[]) {
   string mode("mcideal");
   for (int i = 0; i < argc; i++){
     if (!strcmp(argv[i], "-j"))  {jsondir = argv[++i];;}
+    if (!strcmp(argv[i], "-m"))  {mode    = argv[++i];;}
   }
 
   // -- check whether directories for JSONs already exist
@@ -84,14 +90,14 @@ int main(int argc, char* argv[]) {
 	// -- global tags
 	// ----------------------------------------------------------------------
 	map<string, vector<string>> iniGlobalTags = {
-		{"mdc2023",
-     {"pixelalignment_mdc2023", "pixelquality_mdc2023", "pixelcablingmap_mdc2023"}
-    }
+		{"intrun", {"pixelalignment_intrun", "pixelquality_intrun", "pixelcablingmap_intrun"} },
+		{"mcideal", {"pixelalignment_mcideal", "fibrealignment_mcideal", "tilealignment_mcideal", "mppcalignment_mcideal", "pixelquality_mcideal"} }
   };
 	
 	string jdir  = jsondir + "/globaltags";
   
   for (auto igt : iniGlobalTags) {
+    if (string::npos == igt.first.find(mode)) continue;
 		auto array_builder = bsoncxx::builder::basic::array{};
 		for (auto it : igt.second) array_builder.append(it);
 		bsoncxx::document::value doc_value = builder
@@ -112,14 +118,18 @@ int main(int argc, char* argv[]) {
 	// ----------------------------------------------------------------------
 	// -- iovs
 	// ----------------------------------------------------------------------
-  
 	map<string, vector<int>> iniIovs = {
-		{"pixelalignment_intrun", {1}}, {"pixelquality_intrun", {1}}, {"pixelcablingmap_intrun", {1}}
+    // -- intrun
+		{"pixelalignment_intrun", {1}}, {"pixelquality_intrun", {1}}, {"pixelcablingmap_intrun", {1}},
+    // -- mcideal
+		{"pixelalignment_mcideal", {1}}, {"fibrealignment_mcideal", {1}}, {"tilealignment_mcideal", {1}}, {"mppcalignment_mcideal", {1}},
+    {"pixelquality_intrun", {1}}     // ,{"pixelcablingmap_intrun", {1}}
 	};
 
 	jdir  = jsondir + "/iovs";
 
 	for (auto iiov : iniIovs) {
+    if (string::npos == iiov.first.find(mode)) continue;
 		auto array_builder = bsoncxx::builder::basic::array{};
 		for (auto it : iiov.second) array_builder.append(it);
 		bsoncxx::document::value doc_value = builder
@@ -143,61 +153,96 @@ int main(int argc, char* argv[]) {
   payload pl;
 	jdir = jsondir + "/payloads";
 
-  // -- pixelcablingmap
-  calPixelCablingMap *ccm = new calPixelCablingMap();
-  ccm->readJson("../ascii/pixelcablingmap-intrun.json");
-  string spl = ccm->makeBLOB();
-  string hash("tag_pixelcablingmap_mdc2023_iov_1");
-  
-  pl.fHash = hash; 
-  pl.fComment = "mdc2023 initialization";
-  pl.fBLOB = spl;
-  ccm->printBLOB(spl); 
-  ccm->writePayloadToFile(hash, jdir, pl); 
-  
-  // -- pixelalignment
-  calPixelAlignment *cpa = new calPixelAlignment();
-  cpa->readCsv("../ascii/sensors-mdc2023.csv");
-  spl = cpa->makeBLOB();
-  hash = string("tag_pixelalignment_mdc2023_iov_1");
+  vector<string> tags{"intrun", "mcideal"};
+  string spl(""), hash(""), result("");
 
-  pl.fHash = hash; 
-  pl.fComment = "mdc2023 initialization";
-  pl.fBLOB = spl;
-  cpa->printBLOB(spl); 
-  cpa->writePayloadToFile(hash, jdir, pl); 
+  for (auto it: tags) {
+    if (string::npos == it.find(mode)) continue;
+    
+    // -- pixelalignment
+    calPixelAlignment *cpa = new calPixelAlignment();
+    result = cpa->readCsv("../ascii/sensors-" + it + ".csv");
+    if (string::npos == result.find("Error")) {
+      spl = cpa->makeBLOB();
+      hash = string("tag_pixelalignment_" + it + "_iov_1");
+      pl.fHash = hash; 
+      pl.fComment = it + " pixel initialization";
+      pl.fBLOB = spl;
+      cpa->printBLOB(spl); 
+      cpa->writePayloadToFile(hash, jdir, pl); 
+    }
+    
+    // -- fibrealignment
+    calFibreAlignment *cfa = new calFibreAlignment();
+    result = cfa->readCsv("../ascii/fibres-" + it + ".csv");
+    if (string::npos == result.find("Error")) {
+      spl = cfa->makeBLOB();
+      hash = string("tag_fibrealignment_" + it + "_iov_1");
+      pl.fHash = hash; 
+      pl.fComment = it + " fibre detector initialization";
+      pl.fBLOB = spl;
+      cfa->printBLOB(spl); 
+      cfa->writePayloadToFile(hash, jdir, pl); 
+    }
+    
+    // -- tilealignment
+    calTileAlignment *cta = new calTileAlignment();
+    result = cta->readCsv("../ascii/tiles-" + it + ".csv");
+    if (string::npos == result.find("Error")) {
+      spl = cta->makeBLOB();
+      hash = string("tag_tilealignment_" + it + "_iov_1");
+      pl.fHash = hash; 
+      pl.fComment = it + " tile detector initialization";
+      pl.fBLOB = spl;
+      cta->printBLOB(spl); 
+      cta->writePayloadToFile(hash, jdir, pl); 
+    }
+    
+    // -- mppcalignment
+    calMppcAlignment *cma = new calMppcAlignment();
+    result = cma->readCsv("../ascii/mppcs-" + it + ".csv");
+    if (string::npos == result.find("Error")) {
+      spl = cma->makeBLOB();
+      hash = string("tag_mppcalignment_" + it + "_iov_1");
+      pl.fHash = hash; 
+      pl.fComment = it + " MPPC detector initialization";
+      pl.fBLOB = spl;
+      cma->printBLOB(spl); 
+      cma->writePayloadToFile(hash, jdir, pl); 
+    }
+    
+    // -- pixelcablingmap
+    calPixelCablingMap *ccm = new calPixelCablingMap();
+    result = ccm->readJson("../ascii/pixelcablingmap-" + it + ".json");
+    if (string::npos == result.find("Error")) {
+      spl = ccm->makeBLOB();
+      hash = "tag_pixelcablingmap_" + it + "_iov_1";
+      pl.fHash = hash; 
+      pl.fComment = it + "pixel cabling map initialization";
+      pl.fBLOB = spl;
+      ccm->printBLOB(spl); 
+      ccm->writePayloadToFile(hash, jdir, pl); 
+    }
 
-  // -- pixelquality: zero problematic pixels for all sensors present in cpa
-  calPixelQuality *cpq = new calPixelQuality();
-  unsigned int uid(999999);
-  map<unsigned int, vector<double> > m;
-  while (cpa->getNextID(uid)) {
-    vector<double> v;
-    m.insert(make_pair(uid, v));
-    // cout << "sensor = " << uid << " vector size = " << v.size() << endl;
+    // -- pixelquality: zero problematic pixels for all sensors present in cpa
+    calPixelQuality *cpq = new calPixelQuality();
+    unsigned int uid(999999);
+    map<unsigned int, vector<double> > m;
+    while (cpa->getNextID(uid)) {
+      vector<double> v;
+      m.insert(make_pair(uid, v));
+      // cout << "sensor = " << uid << " vector size = " << v.size() << endl;
+    }
+    spl = cpq->makeBLOB(m);
+    hash = string("tag_pixelquality_" + it + "_iov_1");
+    
+    pl.fHash = hash; 
+    pl.fComment = it + " pixel quality initialization";
+    pl.fBLOB = spl;
+    cpq->printBLOB(spl); 
+    cpq->writePayloadToFile(hash, jdir, pl); 
+
   }
-  spl = cpq->makeBLOB(m);
-  hash = string("tag_pixelquality_mdc2023_iov_1");
 
-  pl.fHash = hash; 
-  pl.fComment = "mdc2023 initialization";
-  pl.fBLOB = spl;
-  cpq->printBLOB(spl); 
-  cpq->writePayloadToFile(hash, jdir, pl); 
-
-
-  // -- fibrealignment
-  calFibreAlignment *cfa = new calFibreAlignment();
-  cfa->readCsv("../ascii/fibres-full.csv");
-  spl = cfa->makeBLOB();
-  hash = string("tag_fibrealignment_mdc2023_iov_1");
-
-  pl.fHash = hash; 
-  pl.fComment = "full fibre detector initialization";
-  pl.fBLOB = spl;
-  cfa->printBLOB(spl); 
-  cfa->writePayloadToFile(hash, jdir, pl); 
-
-  
 	return 0;
 }
