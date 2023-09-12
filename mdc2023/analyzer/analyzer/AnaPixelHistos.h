82a60c0c949e4fc3d76f30ecfe472ce50360c95a
Urs Langenegger <urslangenegger@gmail.com>
2023-09-12 16:08:56 +0200

    fill pixel hit map

--- mdc2023/analyzer/analyzer/AnaPixelHistos.h
+++ mdc2023/analyzer/analyzer/AnaPixelHistos.h
@@ -9,44 +9,42 @@ class TH2F;
 class TH2D;
 
 class AnaPixelHistos: public TARunObject {
-    public:
-    AnaPixelHistos(TARunInfo* runinfo);
-    ~AnaPixelHistos();
-    void BeginRun(TARunInfo* runinfo);
-    void EndRun(TARunInfo* runinfo);
-    TAFlowEvent* Analyze(TARunInfo*, TMEvent*, 
-        TAFlags* flags, TAFlowEvent* flow){
-            // This function doesn't analyze anything, so we use flags 
-            // to have the profiler ignore it
-            *flags |= TAFlag_SKIP_PROFILE;
-            return flow;
-        };
-     TAFlowEvent* AnalyzeFlowEvent(TARunInfo*, 
-        TAFlags* flags, TAFlowEvent* flow);
+public:
+  AnaPixelHistos(TARunInfo* runinfo);
+  ~AnaPixelHistos();
+  void BeginRun(TARunInfo* runinfo);
+  void EndRun(TARunInfo* runinfo);
+  TAFlowEvent* Analyze(TARunInfo*, TMEvent*, TAFlags* flags, TAFlowEvent* flow) {
+    // This function doesn't analyze anything, so we use flags 
+    // to have the profiler ignore it
+    *flags |= TAFlag_SKIP_PROFILE;
+    return flow;
+  };
+  TAFlowEvent* AnalyzeFlowEvent(TARunInfo*, TAFlags* flags, TAFlowEvent* flow);
 
-    protected:
+protected:
     
-    TFile* dataFile{};
-    TH1F * chipID{};
+  TFile* dataFile{};
+  TH1F * chipID{};
+  std::map<unsigned int, TH2F*> hPixelHitMap{};
+  int fEvtCounter{0};
 };
 
 // Because we have the equipment in the constructor, 
 // we have to create a non-defaultfactory for this analyzer module
 
-class AnaPixelHistoFactory: public TAFactory
-{
+class AnaPixelHistoFactory: public TAFactory {
 public:
 
-    void Usage(){}
+  void Usage(){}
 
-    void Init(const std::vector<std::string> &args);
+  void Init(const std::vector<std::string> &args);
 
-    void Finish();
+  void Finish();
    
-    AnaPixelHistos* NewRunObject(TARunInfo* runinfo)
-    {
-        return new AnaPixelHistos(runinfo);
-    }
+  AnaPixelHistos* NewRunObject(TARunInfo* runinfo)  {
+    return new AnaPixelHistos(runinfo);
+  }
 };
 
 
