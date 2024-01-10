faa21a1ff1159f42181101859bac91f367306514
Urs Langenegger <urslangenegger@gmail.com>
2024-01-10 15:20:09 +0100

    move from posts to cdb

--- db1/rest/index.mjs
+++ db1/rest/index.mjs
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import "express-async-errors";
-import posts from "./routes/posts.mjs";
+import cdb from "./routes/cdb.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Load the /posts routes
-app.use("/posts", posts);
+app.use("/cdb", cdb);
 
 // Global error handling
 app.use((err, _req, res, next) => {
