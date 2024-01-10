a07fc4895e27adfbd0940d2f10b2f3a98135f281
Urs Langenegger <urslangenegger@gmail.com>
2024-01-10 11:43:38 +0100

    add console.log()

--- db1/rest/routes/posts.mjs
+++ db1/rest/routes/posts.mjs
@@ -4,29 +4,9 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-// Get a list of 50 posts
-router.get("/", async (req, res) => {
-  let collection = await db.collection("runrecords");
-  let results = await collection.find({})
-    .limit(50)
-    .toArray();
-
-  res.send(results).status(200);
-});
-
-// Fetches the latest posts
-router.get("/latest", async (req, res) => {
-  let collection = await db.collection("runrecords");
-  let results = await collection.aggregate([
-    {"$project": {"author": 1, "title": 1, "tags": 1, "date": 1}},
-    {"$sort": {"date": -1}},
-    {"$limit": 3}
-  ]).toArray();
-  res.send(results).status(200);
-});
-
 // Get a single runrecord
-router.get("/rr/:id", async (req, res) => {
+router.get("/findOne/runrecords/:id", async (req, res) => {
+  console.log("serving /findOne/runrecords/" + req.params.id);
   let collection = await db.collection("runrecords");
   let query = {run: req.params.id};
   let result = await collection.findOne(query);
@@ -36,7 +16,8 @@ router.get("/rr/:id", async (req, res) => {
 });
 
 // Get a single global tag
-router.get("/gt/:id", async (req, res) => {
+router.get("/findOne/globaltags/:id", async (req, res) => {
+  console.log("serving /findOne/globaltags/" + req.params.id);
   let collection = await db.collection("globaltags");
   let query = {gt: req.params.id};
   let result = await collection.findOne(query);
@@ -46,23 +27,36 @@ router.get("/gt/:id", async (req, res) => {
 });
 
 // Get a single tag/IOV
-router.get("/tag/:id", async (req, res) => {
+router.get("/findOne/tags/:id", async (req, res) => {
+  console.log("serving /findOne/tags/" + req.params.id);
   let collection = await db.collection("tags");
   let query = {tag: req.params.id};
   let result = await collection.findOne(query);
-
   if (!result) res.send("Not found").status(404);
   else res.send(result).status(200);
 });
 
 // Get a single payload
-router.get("/payload/:id", async (req, res) => {
+router.get("/findOne/payloads/:id", async (req, res) => {
+  console.log("serving /findOne/payloads/" + req.params.id);
   let collection = await db.collection("payloads");
   let query = {hash: req.params.id};
   let result = await collection.findOne(query);
 
   if (!result) res.send("Not found").status(404);
   else res.send(result).status(200);
+});
+
+
+// Get all globaltags
+router.get("/findAll/globaltags", async (req, res) => {
+  console.log("serving /findAll/globaltags/" + req.params.id);
+  let collection = await db.collection("globaltags");
+  let results = await collection.find({})
+    .limit(50)
+    .toArray();
+
+  res.send(results).status(200);
 });
 
 
