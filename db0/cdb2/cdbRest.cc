a46f80747963c0210f0a37356c56c863c048b859
Urs Langenegger <urslangenegger@gmail.com>
2024-01-11 13:42:32 +0100

    move include to src

--- db0/cdb2/cdbRest.cc
+++ db0/cdb2/cdbRest.cc
@@ -6,30 +6,10 @@
 #include <sstream>
 #include <dirent.h>    /// for directory reading
 
-#include <bsoncxx/json.hpp>
-#include <mongocxx/client.hpp>
-#include <mongocxx/stdx.hpp>
-#include <mongocxx/uri.hpp>
-#include <bsoncxx/builder/stream/helpers.hpp>
-#include <bsoncxx/builder/stream/document.hpp>
-#include <bsoncxx/builder/stream/document.hpp>
-#include <bsoncxx/builder/stream/array.hpp>
+#include <curl/curl.h> 
 
 #include "base64.hh"
 #include "cdbUtil.hh"
-
-using bsoncxx::builder::basic::kvp;
-using bsoncxx::builder::stream::close_array;
-using bsoncxx::builder::stream::close_document;
-using bsoncxx::builder::stream::document;
-using bsoncxx::builder::stream::finalize;
-using bsoncxx::builder::stream::open_array;
-using bsoncxx::builder::stream::open_document;
-using bsoncxx::builder::basic::sub_array;
-using bsoncxx::builder::basic::sub_document;
-using bsoncxx::builder::basic::make_document;
-
-
 
 using namespace std;
 
