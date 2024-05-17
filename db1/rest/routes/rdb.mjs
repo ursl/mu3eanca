7a232e1dad00c95811e9145e5724fd4d5bbd8fb8
Urs Langenegger <urslangenegger@gmail.com>
2024-05-17 09:38:52 +0200

    add single run route

--- db1/rest/routes/rdb.mjs
+++ db1/rest/routes/rdb.mjs
@@ -5,6 +5,22 @@ import { ObjectId } from "mongodb";
 const router = express.Router();
 
 // -- Get a single runrecord
+router.get("/", async (req, res) => {
+    console.log("render index4");
+    let collection = await db.collection("runrecords");
+
+    const options = {
+        // Sort returned documents in ascending order by title (A->Z)
+        sort: { "BOR.Run number": -1 },
+        limit: {$eq: 5}
+    };
+    let query = { };
+    const result = await collection.find(query, options).limit(20).toArray();
+    console.log(JSON.stringify(result));
+    res.render('index', {'data': result});
+});
+
+// -- Get a single runrecord
 router.get("/:id", async (req, res) => {
     let runno = parseInt(req.params.id);
     console.log("serving ... /rdb/" + req.params.id + " from " + req.ip);
@@ -16,13 +32,8 @@ router.get("/:id", async (req, res) => {
 
     if (!result) res.send("Not found").status(404);
     else {
-        const data = {
-            name: "jaimin",
-            email: "jaimin@gmail.com"
-        };
-
-        console.log("calling render");
-        res.render('index', data);
+        console.log("calling singleRun with result: " + JSON.stringify(result));
+        res.render('singleRun', {'data': result});
     }
 });
 
