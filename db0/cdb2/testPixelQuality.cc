b2fb5bc633d1fba4106c6453fe655986ebc86d24
Urs Langenegger <urslangenegger@gmail.com>
2024-05-22 15:51:06 +0200

    remove fake left-over code

--- db0/cdb2/testPixelQuality.cc
+++ db0/cdb2/testPixelQuality.cc
@@ -22,33 +22,13 @@
 #include "TRandom3.h"
 #include "TMath.h"
 
-#include <bsoncxx/json.hpp>
-#include <mongocxx/client.hpp>
-#include <mongocxx/stdx.hpp>
-#include <mongocxx/uri.hpp>
-#include <bsoncxx/builder/stream/helpers.hpp>
-#include <bsoncxx/builder/stream/document.hpp>
-#include <bsoncxx/builder/stream/document.hpp>
-#include <bsoncxx/builder/stream/array.hpp>
-
 using namespace std;
-
-using bsoncxx::builder::stream::close_array;
-using bsoncxx::builder::stream::close_document;
-using bsoncxx::builder::stream::document;
-using bsoncxx::builder::stream::finalize;
-using bsoncxx::builder::stream::open_array;
-using bsoncxx::builder::stream::open_document;
-using bsoncxx::builder::basic::kvp;
-using bsoncxx::builder::basic::sub_array;
-using bsoncxx::builder::basic::sub_document;
-using bsoncxx::builder::basic::make_document;
 
 // ----------------------------------------------------------------------
 // testPixelQuality
 // ----------------
 //
-// Examples: 
+// Examples:
 // bin/testPixelQuality -n 10 -noisy1 100 -noisy2 1100 -nrec1 1000
 // bin/testPixelQuality -n 10 -noisy1 100 -noisy2 1100 -nrec1 500
 // bin/testPixelQuality -n 10 -noisy1 100 -noisy2 1100 -nrec1 50
@@ -73,7 +53,7 @@ void createRandomHits(map<unsigned int, vector<pixhit> > &, int, int);
 int main(int argc, char* argv[]) {
 
   int NCHIPS(1), NNOISY(2), NRECCHIPS(NCHIPS), NRECHITS(200);
-  
+
   // -- command line arguments
   int verbose(0), mode(1), nevts(2);
   unsigned long int rseed(123456);
@@ -94,15 +74,15 @@ int main(int argc, char* argv[]) {
   }
 
   gRandom->SetSeed(rseed);
-  
+
   string gt("mcideal");
   cdbAbs *pDB = new cdbJSON(gt, "json", verbose);
-  
+
   Mu3eConditions *pDC = Mu3eConditions::instance(gt, pDB);
   pDC->setVerbosity(verbose);
 
   int nstep (10);
-  
+
   TCanvas c1;
   long long totalTime(0);
   TH1D *hTime;
@@ -131,7 +111,7 @@ int main(int argc, char* argv[]) {
   grSetup->SetMarkerStyle(20);
   grSetup->SetMarkerSize(1.5);
 
-  calAbs *cpq(0); 
+  calAbs *cpq(0);
   int inc = (noisy2-noisy1)/nstep;
   for (int inoise = noisy1; inoise <= noisy2; inoise += inc) {
     cout << "######################################################################" << endl;
@@ -146,7 +126,7 @@ int main(int argc, char* argv[]) {
     hSetup->Reset();
     for (int ievt = 0; ievt < nevts; ++ievt) {
       cout << "####### evt " << ievt << endl;
-      
+
       if (1 == mode) {
         cpq = new calPixelQuality();
       } else if (2 == mode) {
@@ -158,20 +138,20 @@ int main(int argc, char* argv[]) {
       auto sbegin = std::chrono::high_resolution_clock::now();
       string hash("tag_pixelquality_mcideal_iov_1");
       createPayload(hash, cpq, nchips, inoise);
-      
+
       cpq->readPayloadFromFile(hash, ".");
       cpq->calculate(hash);
 
-      map<unsigned int, vector<pixhit>> detHits; 
+      map<unsigned int, vector<pixhit>> detHits;
       createRandomHits(detHits, NRECCHIPS, nrec1);
       auto send = std::chrono::high_resolution_clock::now();
       long long duss = chrono::duration_cast<chrono::milliseconds>(send-sbegin).count();
       hSetup->Fill(static_cast<double>(duss));
-      
+
       // -- now loop over all rec hits
       auto tbegin = std::chrono::high_resolution_clock::now();
       for (auto it: detHits) {
-        vector<pixhit> v = it.second; 
+        vector<pixhit> v = it.second;
         //        cout << "it.first = " << it.first << " npix = " << v.size() << endl;
         for (auto ipix: v) {
           if (cpq->getStatus(ipix.ichip, ipix.icol, ipix.irow)) {
@@ -192,7 +172,7 @@ int main(int argc, char* argv[]) {
          << " # NCHIPS/NNOISY/NRECHITS = "
          << nchips << "/" << inoise << "/" << nrec1 << " (nrec2 = " << nrec2 << ")"
          << endl;
-    
+
     hTime->SetTitle(Form("timing mode%d-nchips%d-nnoise%d-nrec%d", mode, nchips, inoise, nrec1));
     hTime->Draw();
     c1.SaveAs(Form("hTime-mode%d-nchips%d-nnoise%d-nrec%d.pdf", mode, nchips, inoise, nrec1));
@@ -200,8 +180,8 @@ int main(int argc, char* argv[]) {
     hSetup->SetTitle(Form("setup mode%d-nchips%d-nnoise%d-nrec%d", mode, nchips, inoise, nrec1));
     hSetup->Draw();
     c1.SaveAs(Form("hSetup-mode%d-nchips%d-nnoise%d-nrec%d.pdf", mode, nchips, inoise, nrec1));
-    
-    // - this works for ROOT >= 6.24 
+
+    // - this works for ROOT >= 6.24
     //    grSetup->AddPoint(inoise, hSetup->GetMean());
     //    grNoise->AddPoint(inoise, hTime->GetMean());
     grSetup->SetPoint(grSetup->GetN(), inoise, hSetup->GetMean());
@@ -223,27 +203,27 @@ int main(int argc, char* argv[]) {
 // ----------------------------------------------------------------------
 string writeBlob(string filename, int nchip, int nnoisy) {
   map<unsigned int, vector<double> > mdet;
-  
+
   long unsigned int header(0xdeadface);
   stringstream ONS;
   if (1) {
     ONS << dumpArray(uint2Blob(header));
   }
-  
+
   if (1) cout << " nchip = " << nchip
               << " nnoisy = " << nnoisy
               << endl;
   for (int i = 0; i < nchip; ++i) {
     vector<double> vchip{};
     int nnoisypix(nnoisy);
-    ONS << dumpArray(uint2Blob(i)) 
+    ONS << dumpArray(uint2Blob(i))
         << dumpArray(int2Blob(nnoisypix));
     vchip.push_back(static_cast<double>(nnoisypix));
-    
+
     for (int ipix = 0; ipix < nnoisypix; ++ipix) {
       int icol = 100 + 50*gRandom->Rndm();
       int irow = 120 + 50*gRandom->Rndm();
-      char iqual = 1; 
+      char iqual = 1;
       ONS << dumpArray(int2Blob(icol))
           << dumpArray(int2Blob(irow))
           << dumpArray(uint2Blob(static_cast<unsigned int>(iqual)));
@@ -252,7 +232,7 @@ string writeBlob(string filename, int nchip, int nnoisy) {
       vchip.push_back(static_cast<double>(iqual));
 
       if (0) cout << "icol/irow = " << icol << "/" << irow
-                  << " qual = " << static_cast<unsigned int>(iqual) 
+                  << " qual = " << static_cast<unsigned int>(iqual)
                   << endl;
     }
     mdet.insert(make_pair(i, vchip));
@@ -270,23 +250,23 @@ void dumpBlob(string filename, int nchip, int nnoisy) {
   if (1) {
     ONS << dumpArray(uint2Blob(header));
   }
-  
+
   if (1) cout << " nchip = " << nchip
               << " nnoisy = " << nnoisy
               << endl;
   for (int i = 0; i < nchip; ++i) {
     int nnoisypix(nnoisy);
-    ONS << dumpArray(uint2Blob(i)) 
+    ONS << dumpArray(uint2Blob(i))
         << dumpArray(int2Blob(nnoisypix));
     for (int ipix = 0; ipix < nnoisypix; ++ipix) {
       int icol = 100 + 50*gRandom->Rndm();
       int irow = 120 + 50*gRandom->Rndm();
-      char iqual = 1; 
+      char iqual = 1;
       ONS << dumpArray(int2Blob(icol))
           << dumpArray(int2Blob(irow))
           << dumpArray(uint2Blob(static_cast<unsigned int>(iqual)));
       if (0) cout << "icol/irow = " << icol << "/" << irow
-                  << " qual = " << static_cast<unsigned int>(iqual) 
+                  << " qual = " << static_cast<unsigned int>(iqual)
                   << endl;
     }
   }
@@ -295,7 +275,7 @@ void dumpBlob(string filename, int nchip, int nnoisy) {
 }
 
 
-// ----------------------------------------------------------------------  
+// ----------------------------------------------------------------------
 void createPayload(string hash, calAbs *a, int nchips, int nnoisy) {
   string tmpfilename("bla");
 
@@ -307,12 +287,12 @@ void createPayload(string hash, calAbs *a, int nchips, int nnoisy) {
     ONS.open(tmpfilename);
     ONS << blobs;
     ONS.close();
-    
+
     map<unsigned int, vector<double> > mdet = a->decodeBLOB(blobs);
     string sdet = a->makeBLOB(mdet);
     cout << " a->makeBLOB(mdet): " << endl;
   }
-  
+
   std::ifstream file;
   file.open(tmpfilename);
   vector<char> buffer(std::istreambuf_iterator<char>(file), {});
@@ -323,7 +303,7 @@ void createPayload(string hash, calAbs *a, int nchips, int nnoisy) {
   pl.fHash = hash;
   pl.fComment = "testing";
   pl.fBLOB = sblob;
-  a->writePayloadToFile(hash, ".", pl); 
+  a->writePayloadToFile(hash, ".", pl);
 }
 
 
@@ -334,10 +314,10 @@ void createRandomHits(map<unsigned int, vector<pixhit> > &dethits, int nrecchips
   if (1) cout << " nrecchips = " << nrecchips
               << " nrechits = " << nrechits
               << endl;
-  
+
   pixhit a;
   for (int ic = 0; ic < nrecchips; ++ic) {
-    vector<pixhit> v; 
+    vector<pixhit> v;
     for (int ih = 0; ih < nrechits; ++ih) {
       a.ichip = ic;
       a.icol = 100 + 60*gRandom->Rndm();
