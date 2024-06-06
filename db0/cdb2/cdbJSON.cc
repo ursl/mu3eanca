5616b5db9aa7c6a9998b71a29fe0c35a92af2116
Urs Langenegger <urslangenegger@gmail.com>
2024-06-06 11:03:04 +0200

    read all fields

--- db0/cdb2/cdbJSON.cc
+++ db0/cdb2/cdbJSON.cc
@@ -36,7 +36,7 @@ vector<string> cdbJSON::readGlobalTags() {
   string gtdir = fURI + "/globaltags";
   cout << "cdbJSON::readGlobalTags() from  gtdir = " << gtdir << endl;
   vector<string> gtFiles = allFiles(gtdir);
-
+  
   ifstream INS;
   for (auto it: gtFiles) {
     // -- remove everything up to and including the last /
@@ -54,7 +54,7 @@ vector<string> cdbJSON::readTags(string gt) {
   vector<string> v;
   // -- read global tags from fURI
   string gtdir = fURI + "/globaltags/";
-
+  
   ifstream INS;
   string gtfile = gtdir + gt;
   INS.open(gtfile);
@@ -62,18 +62,18 @@ vector<string> cdbJSON::readTags(string gt) {
     cout << "Error failed to open ->" << gtfile << "<-" << endl;
     return v;
   }
-
+  
   std::stringstream buffer;
   buffer << INS.rdbuf();
   INS.close();
-
+  
   string lBuffer = buffer.str();
-
+  
   vector<string> subarr = split(jsonGetVector(lBuffer, "tags"), ',');
   for (auto it: subarr) {
     v.push_back(it);
   }
-
+  
   if (fVerbose > 0) {
     cout << "cdbJSON::readTags> for GT = " << gt << endl;
     print(v);
@@ -85,11 +85,11 @@ vector<string> cdbJSON::readTags(string gt) {
 // ----------------------------------------------------------------------
 map<string, vector<int>> cdbJSON::readIOVs(vector<string> tags) {
   map<string, vector<int>> m;
-
+  
   // -- read iovs from fURI
   ifstream INS;
   string dir = fURI + "/tags/";
-
+  
   for (auto it: tags) {
     string file = dir + it;
     INS.open(file);
@@ -97,16 +97,16 @@ map<string, vector<int>> cdbJSON::readIOVs(vector<string> tags) {
       cout << "Error failed to open ->" << file << "<-" << endl;
       return m;
     }
-
+    
     std::stringstream buffer;
     buffer << INS.rdbuf();
     INS.close();
-
+    
     string lBuffer = buffer.str();
-
+    
     vector<int> viov;
     string sarr = jsonGetVector(lBuffer, "iovs");
-
+    
     vector<string> subarr = split(sarr, ',');
     if (subarr.size() > 0) {
       for (auto it: subarr) {
@@ -117,7 +117,7 @@ map<string, vector<int>> cdbJSON::readIOVs(vector<string> tags) {
     }
     m.insert(make_pair(it, viov));
   }
-
+  
   return m;
 }
 
@@ -130,25 +130,25 @@ runRecord cdbJSON::getRunRecord(int irun) {
        << " not found)";
   runRecord rr;
   rr.fEORComments = sspl.str();
-
+  
   // -- read runRecord for run irun
   ifstream INS;
   //  string filename = fURI + "/runrecords/" + to_string(irun);
   std::ostringstream oss;
   oss << "runlog_" << std::setfill('0') << std::setw(6) << irun << ".json";
   string filename = fURI + "/runrecords/" + oss.str();
-
-
+  
+  
   INS.open(filename);
   if (INS.fail()) {
     cout << "Error failed to open ->" << filename << "<-" << endl;
     return rr;
   }
-
+  
   std::stringstream buffer;
   buffer << INS.rdbuf();
   INS.close();
-
+  
   cout << "cdbJSON::getRunRecord() Read " << filename << endl;
   string jstring = buffer.str();
   rr.fBORRunNumber     = stoi(jsonGetValue(jstring, "Run number"));
@@ -156,15 +156,15 @@ runRecord cdbJSON::getRunRecord(int irun) {
   rr.fBORSubsystems    = stoi(jsonGetValue(jstring, "Subsystems"));
   rr.fBORBeam          = stof(jsonGetValue(jstring, "Beam"));
   rr.fBORShiftCrew     = jsonGetString(jstring, "Shift crew");
-
+  
   rr.fEORStopTime      = jsonGetString(jstring, "Stop time");
   rr.fEOREvents        = stoi(jsonGetValue(jstring, "Events"));
   rr.fEORFileSize      = stod(jsonGetValue(jstring, "File size"));
   rr.fEORDataSize      = stod(jsonGetValue(jstring, "Uncompressed data size"));
   rr.fEORComments      = jsonGetString(jstring, "Comments");
-
+  
   rr.fConfigurationKey = jsonGetString(jstring, "Configuration key");
-
+  
   return rr;
 }
 
@@ -176,7 +176,7 @@ cfgPayload cdbJSON::getConfig(string hash) {
   sspl << "(cdbJSON>  config for hash = " << hash
        << " not found)";
   cfgPayload cfg;
-
+  
   // -- read config
   ifstream INS;
   string filename = fURI + "/configs/" + hash;
@@ -185,16 +185,16 @@ cfgPayload cdbJSON::getConfig(string hash) {
     cout << "Error failed to open ->" << filename << "<-" << endl;
     return cfg;
   }
-
+  
   std::stringstream buffer;
   buffer << INS.rdbuf();
   INS.close();
-
+  
   string jstring = buffer.str();
   cfg.fHash      = jsonGetString(jstring, "cfgHash");
   cfg.fDate      = jsonGetString(jstring, "cfgDate");
   cfg.fCfgString = jsonGetCfgStringEsc(jstring, "cfgString");
-
+  
   return cfg;
 }
 
@@ -207,7 +207,7 @@ payload cdbJSON::getPayload(string hash) {
        << " not found)";
   payload pl;
   pl.fComment = sspl.str();
-
+  
   // -- read payload for hash
   ifstream INS;
   string filename = fURI + "/payloads/" + hash;
@@ -216,19 +216,20 @@ payload cdbJSON::getPayload(string hash) {
     cout << "Error failed to open ->" << filename << "<-" << endl;
     return pl;
   }
-
+  
   std::stringstream buffer;
   buffer << INS.rdbuf();
   INS.close();
-
+  
   cout << "cdbJSON::getPayload() Read " << filename << endl;
-
+  
   string jstring = buffer.str();
   pl.fHash       = jsonGetString(jstring, "hash");
   pl.fComment    = jsonGetString(jstring, "comment");
   pl.fSchema     = jsonGetString(jstring, "schema");
+  pl.fDate       = jsonGetString(jstring, "date");
   pl.fBLOB       = base64_decode(jsonGetString(jstring, "BLOB"));
-
+  
   return pl;
 }
 
@@ -238,20 +239,20 @@ vector<string> cdbJSON::allFiles(string dirName) {
   vector<string> vfiles;
   DIR *folder;
   struct dirent *entry;
-
+  
   folder = opendir(dirName.c_str());
   if (folder == NULL) {
     puts("Unable to read directory");
     return vfiles;
   }
-
+  
   while ((entry=readdir(folder))) {
     if (8 == entry->d_type) {
       vfiles.push_back(dirName + "/" + entry->d_name);
     }
   }
   closedir(folder);
-
+  
   sort(vfiles.begin(), vfiles.end());
   return vfiles;
 }
