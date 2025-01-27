f1dc9346222d7cf045816f3d0a9991b03ea7aa2c
Urs Langenegger <urslangenegger@gmail.com>
2025-01-27 10:33:58 +0100

    complete layer result

--- ana/trGen.cc
+++ ana/trGen.cc
@@ -526,9 +526,20 @@ TVector3  trGen::getHitLocation(uint32_t id) {
 int trGen::pixelLayer(uint32_t id) {
   int pixID = pixelID(id);
   
-  if (pixID < 300) {
+  if (pixID < 231) {
     return 1; 
   } else if (pixID < 1400) {
     return 2; 
-  }
+  } else if (
+             (6152 < pixID && pixID < 6904) 
+             || (2056 < pixID && pixID < 2808)
+             || (10248 < pixID && pixID < 11000)) {
+    return 3; 
+  } else if (
+             (7175 < pixID && pixID < 8056)
+             || (3079 < pixID) && (3960 < pixID) 
+             || (11271 < pixID) && (pixID < 12152)) {
+    return 4; 
+  } 
+  return -1;
 }
