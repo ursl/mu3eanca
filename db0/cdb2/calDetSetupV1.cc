33b1e4e2be024a0f45d0b376f1c775b522ece223
Urs Langenegger <urslangenegger@gmail.com>
2025-04-22 17:32:40 +0200

    migrate to new name (detSetup instead of detConfig, to avoid confusion with detConf)

---
+++ db0/cdb2/calDetSetupV1.cc
@@ -0,0 +1,181 @@
+#include "calDetSetupV1.hh"
+
+#include "cdbUtil.hh"
+
+#include <iostream>
+#include <sstream>
+
+
+using namespace std;
+
+// ----------------------------------------------------------------------
+calDetSetupV1::calDetSetupV1(cdbAbs *db) : calAbs(db) {
+}
+
+
+// ----------------------------------------------------------------------
+calDetSetupV1::calDetSetupV1(cdbAbs *db, string tag) : calAbs(db, tag) {
+  if (fVerbose) cout << "calDetSetupV1 created and registered with tag ->"
+                       << fTag << "<-"
+                       << endl;
+}
+
+
+// ----------------------------------------------------------------------
+calDetSetupV1::~calDetSetupV1() {
+  cout << "this is the end of calDetSetupV1 with tag ->" << fTag << "<-" << endl;
+}
+
+
+// ----------------------------------------------------------------------
+void calDetSetupV1::calculate(string hash) {
+  cout << "calDetSetupV1::calculate() with "
+       << "fHash ->" << hash << "<-";
+  string spl = fTagIOVPayloadMap[hash].fBLOB;
+
+  std::vector<char> buffer(spl.begin(), spl.end());
+  std::vector<char>::iterator ibuffer = buffer.begin();
+
+  unsigned int header = blob2UnsignedInt(getData(ibuffer));
+  cout << "header: " << hex << header << dec << endl;
+
+  fConstants.target.shape = blob2UnsignedInt(getData(ibuffer));
+  fConstants.target.thickness1 = blob2Double(getData(ibuffer));
+  fConstants.target.thickness2 = blob2Double(getData(ibuffer));
+  fConstants.target.length = blob2Double(getData(ibuffer));
+  fConstants.target.radius = blob2Double(getData(ibuffer));
+  fConstants.target.offsetX = blob2Double(getData(ibuffer));
+  fConstants.target.offsetY  = blob2Double(getData(ibuffer));
+  fConstants.target.offsetZ  = blob2Double(getData(ibuffer));
+  fConstants.magnet.fieldStrength = blob2Double(getData(ibuffer));
+
+}
+
+
+// ----------------------------------------------------------------------
+void calDetSetupV1::printBLOB(std::string sblob, int verbosity) {
+
+  std::vector<char> buffer(sblob.begin(), sblob.end());
+  std::vector<char>::iterator ibuffer = buffer.begin();
+
+  unsigned int header = blob2UnsignedInt(getData(ibuffer));
+  cout << "calDetSetupV1::printBLOB(string)" << endl;
+  cout << "   header: " << hex << header << dec << endl;
+
+  if (0 == verbosity) return;
+
+  cout << "target"  << endl
+       << "  .shape = " << blob2UnsignedInt(getData(ibuffer)) << endl
+       << "  .thickness1 = " << blob2Double(getData(ibuffer)) << endl
+       << "  .thickness2 = " << blob2Double(getData(ibuffer)) << endl
+       << "  .length = " << blob2Double(getData(ibuffer)) << endl
+       << "  .radius = " << blob2Double(getData(ibuffer)) << endl
+       << "  .Offset x/y/z = " << blob2Double(getData(ibuffer))
+       << "/" << blob2Double(getData(ibuffer))
+       << "/" << blob2Double(getData(ibuffer)) << endl
+       << "magnet.field" << endl
+       << "  .strength = " << blob2Double(getData(ibuffer)) << endl;
+}
+
+
+// ----------------------------------------------------------------------
+map<unsigned int, vector<double> > calDetSetupV1::decodeBLOB(string spl) {
+  map<unsigned int, vector<double> > vmap;
+
+  std::vector<char> buffer(spl.begin(), spl.end());
+  std::vector<char>::iterator ibuffer = buffer.begin();
+
+  unsigned int header = blob2UnsignedInt(getData(ibuffer));
+  if (0xdeadface != header) {
+    cout << "XXXXX ERRROR in calDetSetupV1::decodeBLOB> header is wrong. Something is really messed up!" << endl;
+  }
+
+  // -- format of m
+  // 0 => target.(shape,thickness1,thickness2,length,radius,offsetX,offsetY,offsetZ),magnet.fieldStrength
+  vector<double> vdet;
+  vdet.push_back(static_cast<double>(blob2UnsignedInt(getData(ibuffer))));
+  vdet.push_back(blob2Double(getData(ibuffer)));
+  vdet.push_back(blob2Double(getData(ibuffer)));
+  vdet.push_back(blob2Double(getData(ibuffer)));
+  vdet.push_back(blob2Double(getData(ibuffer)));
+
+  vdet.push_back(blob2Double(getData(ibuffer)));
+  vdet.push_back(blob2Double(getData(ibuffer)));
+  vdet.push_back(blob2Double(getData(ibuffer)));
+
+  vdet.push_back(blob2Double(getData(ibuffer)));
+
+  vmap.insert(make_pair(0, vdet));
+  return vmap;
+}
+
+
+// ----------------------------------------------------------------------
+string calDetSetupV1::makeBLOB() {
+  stringstream s;
+  unsigned int header(0xdeadface);
+  s << dumpArray(uint2Blob(header));
+
+  s << dumpArray(uint2Blob(fConstants.target.shape));
+  s << dumpArray(double2Blob(fConstants.target.thickness1));
+  s << dumpArray(double2Blob(fConstants.target.thickness2));
+  s << dumpArray(double2Blob(fConstants.target.length));
+  s << dumpArray(double2Blob(fConstants.target.radius));
+  s << dumpArray(double2Blob(fConstants.target.offsetX));
+  s << dumpArray(double2Blob(fConstants.target.offsetY));
+  s << dumpArray(double2Blob(fConstants.target.offsetZ));
+  s << dumpArray(double2Blob(fConstants.magnet.fieldStrength));
+  return s.str();
+}
+
+
+// ----------------------------------------------------------------------
+string calDetSetupV1::makeBLOB(const map<unsigned int, vector<double>>& m) {
+  stringstream s;
+  unsigned int header(0xdeadface);
+  s << dumpArray(uint2Blob(header));
+
+  // -- format of m
+  // 0 => target.(shape,thickness1,thickness2,length,radius,offsetX,offsetY,offsetZ),magnet.fieldStrength
+  for (auto it: m) {
+    s << dumpArray(int2Blob(static_cast<int>(it.second[0])));
+    s << dumpArray(double2Blob(it.second[1]));
+    s << dumpArray(double2Blob(it.second[2]));
+    s << dumpArray(double2Blob(it.second[3]));
+    s << dumpArray(double2Blob(it.second[4]));
+    s << dumpArray(double2Blob(it.second[5]));
+    s << dumpArray(double2Blob(it.second[6]));
+    s << dumpArray(double2Blob(it.second[7]));
+    s << dumpArray(double2Blob(it.second[8]));
+  }
+  return s.str();
+}
+
+
+// ----------------------------------------------------------------------
+string calDetSetupV1::readJSON(string filename) {
+  string spl("");
+  ifstream INS(filename);
+  if (!INS.is_open()) {
+    return "calDetSetupV1::readJSON> Error, file " + filename + " not found";
+  }
+
+  string sline;
+  while (getline(INS, sline)) {
+    replaceAll(sline, "\n", " ");
+    spl += sline;
+  }
+  INS.close();
+
+  fConstants.target.shape         = ::stoi(jsonGetValue(spl, vector<string> {"detector", "target", "shape"}));
+  fConstants.target.thickness1    = ::stod(jsonGetValue(spl, vector<string> {"detector", "target", "thickness1"}));
+  fConstants.target.thickness2    = ::stod(jsonGetValue(spl, vector<string> {"detector", "target", "thickness2"}));
+  fConstants.target.length        = ::stod(jsonGetValue(spl, vector<string> {"detector", "target", "length"}));
+  fConstants.target.radius        = ::stod(jsonGetValue(spl, vector<string> {"detector", "target", "radius"}));
+  fConstants.target.offsetX       = ::stod(jsonGetValue(spl, vector<string> {"detector", "target", "offset", "x"}));
+  fConstants.target.offsetY       = ::stod(jsonGetValue(spl, vector<string> {"detector", "target", "offset", "y"}));
+  fConstants.target.offsetZ       = ::stod(jsonGetValue(spl, vector<string> {"detector", "target", "offset", "z"}));
+  fConstants.magnet.fieldStrength = ::stod(jsonGetValue(spl, vector<string> {"detector", "magnet", "field", "strength"}));
+
+  return spl;
+}
---
+++ db0/cdb2/calDetSetupV1.hh
@@ -0,0 +1,68 @@
+#ifndef CALDETSETUPV1_h
+#define CALDETSETUPV1_h
+
+#include "calAbs.hh"
+
+#include <string>
+#include <map>
+
+// ----------------------------------------------------------------------
+// DetSetup class
+// ----------------------------------------------------------------------
+class calDetSetupV1 : public calAbs {
+public:
+
+  calDetSetupV1() = default;
+  calDetSetupV1(cdbAbs *db);
+  calDetSetupV1(cdbAbs *db, std::string tag);
+  ~calDetSetupV1();
+
+  // -- direct accessors
+  int targetShape() {return fConstants.target.shape;}
+  double targetThickness1() {return fConstants.target.thickness1;}
+  double targetThickness2() {return fConstants.target.thickness2;}
+  double targetLength() {return fConstants.target.length;}
+  double targetRadius() {return fConstants.target.radius;}
+  double targetOffsetX() {return fConstants.target.offsetX;}
+  double targetOffsetY() {return fConstants.target.offsetY;}
+  double targetOffsetZ() {return fConstants.target.offsetZ;}
+
+  double magnetFieldStrength() {return fConstants.magnet.fieldStrength;}
+
+  std::string getName() override {return fDetConfTag;}
+  void        calculate(std::string hash) override;
+
+  std::string makeBLOB() override;
+  std::string makeBLOB(const std::map<unsigned int, std::vector<double>>&) override;
+  std::map<unsigned int, std::vector<double> > decodeBLOB(std::string) override;
+  // -- verbosity = -1 (all), 0 (no elements), n (n elements)
+  void printBLOB(std::string, int verbosity = -1) override;
+  std::string readJSON(std::string filename);
+  std::string getSchema() override {return fSchema;}
+
+private:
+  std::string fDetConfTag{"detconfv1_"};
+
+  // -- local and private
+  struct constants {
+    struct Target {
+      int shape;
+      double thickness1;
+      double thickness2;
+      double length;
+      double radius;
+      double offsetX;
+      double offsetY;
+      double offsetZ;
+    } target;
+    struct Magnet {
+      double fieldStrength;
+    } magnet;
+  };
+
+  std::string fSchema{"target.(shape,thickness1,thickness2,length,radius,offsetX,offsetY,offsetZ),magnet.field.strength"};
+
+  constants fConstants;
+};
+
+#endif
