59919f29d96a01104080d0e1c6da68944fa487ff
Urs Langenegger <urslangenegger@gmail.com>
2025-04-23 15:42:39 +0200

    sync with mu3e code

--- db0/cdb2/calAbs.hh
+++ db0/cdb2/calAbs.hh
@@ -20,15 +20,15 @@ public:
   calAbs(cdbAbs *db);
   calAbs(cdbAbs *db, std::string tag);
   virtual ~calAbs();
-  
+
   void                setIOVs(std::vector<int> v) {fIOVs = v;}
   void                setVerbosity(int v) {fVerbose = v;}
-  virtual std::string getName() {return std::string("blurp");}
+  virtual std::string getName() { return "blurp"; }
   virtual std::string getHash() {return fHash;}
   virtual void        calculate(std::string hash) {
     std::cout << "calAbs::calculate() ?" << std::endl;
   }
-  
+
   // -- BLOB creation from fMapConstants. This is base-class dependent and hence needs to be overridden.
   virtual std::string makeBLOB() {
     return "calAbs::makeBLOB() ?";
@@ -37,38 +37,38 @@ public:
   virtual std::string makeBLOB(const std::map<unsigned int, std::vector<double>>&) {
     return "calAbs::makeBLOB(map<int, vector<double>>) ?";
   }
-  
+
   // -- BLOB decoding. This is base-class dependent and hence needs to be overridden.
-  virtual std::map<unsigned int, std::vector<double>> decodeBLOB(std::string) {
-    return std::map<unsigned int, std::vector<double>> {};
+  virtual std::map<unsigned int, std::vector<double> > decodeBLOB(std::string) {
+    return {};
   }
-  
+
   // -- print the payload. This is base-class dependent and hence needs to be overridden.
   virtual void printBLOB(std::string, int verbosity = 1) {
     std::cout << "calAbs::printBLOB() ?" << std::endl;
   }
-  
+
   virtual std::string  getSchema() {
     return "calAbs::getSchema()?";
   }
-  
+
   // -- direct interactions
   void                readPayloadFromFile(std::string hash, std::string dir);
   void                writePayloadToFile(std::string hash, std::string dir);
   void                writePayloadToFile(std::string hash, std::string dir, payload &pl);
   payload             getPayload(std::string hash) {return fTagIOVPayloadMap[hash];}
   void                insertPayload(std::string hash, payload x);
-  
+
   void                update(std::string hash);
   void                setPrintTiming(int v) {fPrintTiming = v;}
   std::string         getError() {return fError;}
-  
+
   // -- payload string parsing/extraction
   std::string getValue(std::string key);
-  
+
   // -- accessors for various derived classes
   virtual int         getStatus(unsigned int id, int icol, int irow) {return -9999;}
-  
+
 protected:
   cdbAbs                            *fDB;
   std::string                        fTag{"unset"}, fHash{"unset"}, fError{"unset"};
