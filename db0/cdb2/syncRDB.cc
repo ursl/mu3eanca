4059214209938aa556fcbf1ea69688456a8fff0b
Urs Langenegger <urslangenegger@gmail.com>
2025-05-08 17:57:14 +0200

    runInfo Class/Junk/Comments parsed from shift comments field

--- db0/cdb2/syncRDB.cc
+++ db0/cdb2/syncRDB.cc
@@ -1,5 +1,6 @@
 #include "cdbRest.hh"
 #include "runRecord.hh"
+#include "cdbUtil.hh"
 
 #include <iostream>
 #include <fstream>
@@ -9,7 +10,8 @@
 #include <dirent.h>  /// for directory reading
 #include <sys/time.h>
 #include <unistd.h>
-
+#include <cctype> // for toupper()
+#include <algorithm> // for transform()
 
 
 using namespace std;
@@ -20,7 +22,7 @@ using namespace std;
 // -- produce updates to either DataQuality or RunInfo attributes of run records in RDB
 // -- updates a mongoDB server, does not run on the JSON backend server
 // --
-// -- Usage: bin/syncRunDB -m mode -f firstRun -l lastRun 
+// -- Usage: bin/syncRunDB -m mode -f firstRun -l lastRun [-t ../../db1/rest/runInfoTemplate.json] [-u localhost:5050/rdb/addAttribute/]
 // --
 // -- History:
 // --   2025/05/08: first shot
@@ -28,10 +30,17 @@ using namespace std;
 
 void rdbMode1(runRecord &);
 
+
+string runInfoTemplateFile = "../../db1/rest/runInfoTemplate.json";
+vector<string> runInfoTemplateFileLines;
+
+// ----------------------------------------------------------------------
 int main(int argc, char* argv[]) {
 
   // -- command line arguments
-  string urlString("localhost:5050/cdb/");
+  string rdbUpdateString("localhost:5050/rdb/addAttribute");
+  string urlString("localhost:5050/cdb");
+
   bool debug(false);
   int firstRun(0), lastRun(-1), mode(0);
   for (int i = 0; i < argc; i++) {
@@ -39,8 +48,22 @@ int main(int argc, char* argv[]) {
     if (!strcmp(argv[i], "-f"))   {firstRun = atoi(argv[++i]);}
     if (!strcmp(argv[i], "-l"))   {lastRun = atoi(argv[++i]);}
     if (!strcmp(argv[i], "-m"))    {mode    = atoi(argv[++i]);}
+    if (!strcmp(argv[i], "-t"))    {runInfoTemplateFile = string(argv[++i]);}
     if (!strcmp(argv[i], "-u"))    {urlString = string(argv[++i]);}
   }
+
+  // -- read in template
+  ifstream file(runInfoTemplateFile);
+  if (file.is_open()) {
+    string line;
+    while (getline(file, line)) {
+      runInfoTemplateFileLines.push_back(line);
+    }
+    file.close();
+  } else {
+    cerr << "Error: Unable to open file " << runInfoTemplateFile << endl;
+  }
+
 
   cdbRest *pDB(0);
 
@@ -52,19 +75,90 @@ int main(int argc, char* argv[]) {
     if (irun < firstRun) continue;
     if ((lastRun > 0) && (irun > lastRun)) continue;
     
-
     runRecord rr = pDB->getRunRecord(irun);
     if (1 == mode) {
       rdbMode1(rr);
     } 
     if (!debug) {
-      // call curl
+      // curl -X PUT -H "Content-Type: application/json" --data-binary @/Users/ursl/mu3e/mu3eanca/db1/rest/runInfoTemplate.json http://localhost:5050/rdb/addAttribute/513
+      stringstream ss;
+      ss << "curl -X PUT -H \"Content-Type: application/json\" --data-binary @rdb/runInfo_" << irun << ".json " << rdbUpdateString << "/" << irun;
+      cout << "Updating for run number: " << irun << endl; 
+      cout << ss.str() << endl;      
+      system(ss.str().c_str());
     }
   }
   delete pDB; 
 } 
 
-
+// ----------------------------------------------------------------------
+// -- set the following flags by parsing the shift comments
+// -- RunInfo class: source/cosmic/daq
+// -- RunInfo junk:  bad
 void rdbMode1(runRecord &rr) {
-  cout << "hallo: " << rr.printString() << endl;
+
+  vector<string> newRunInfo = runInfoTemplateFileLines;
+  
+  string xstring = rr.fEORComments;
+  transform(xstring.begin(), xstring.end(), xstring.begin(), [](unsigned char c) { return tolower(c); });
+
+  string classFromComments = "not found";
+  string junkFromComments = "not found";
+
+  cout << "run number: " << rr.fBORRunNumber << ": ";
+
+  // -- check for junk indicators stored in vector
+  vector<string> vClassIndicators = {"source", "cosmics", "daq", "calibration", "tuning"};
+  for (const auto &indicator : vClassIndicators) {
+    if (xstring.find(indicator) != string::npos) {
+     cout << " found class indicator: " << indicator;
+      classFromComments = indicator;
+      break;
+    }
+  }
+
+  // -- check for junk indicators stored in vector
+  vector<string> vJunkIndicators = {"bad", "error", "problem", "dbx", "fail", "debug", "test", "dummy"};
+  for (const auto &indicator : vJunkIndicators) {
+    if (xstring.find(indicator) != string::npos) {
+     cout << " and junk indicator: " << indicator;
+      junkFromComments = "true";
+      break;
+    }
+  }
+  cout << endl;
+
+  if (junkFromComments == "not found") {
+    junkFromComments = "false";
+  }
+
+  for (auto &it: newRunInfo) {
+    // -- replace Class value
+    if (it.find("Class") != string::npos) {
+      size_t pos =  it.rfind(":");
+      if (string::npos != pos) {
+        stringstream ss;
+        ss << " \"" << classFromComments << "\",";
+        it.replace(pos + 1, string::npos, ss.str());
+      }
+    }
+    // -- replace Junk value
+    if (it.find("Junk") != string::npos) {
+      size_t pos =  it.rfind(":");
+      if (string::npos != pos) {
+        stringstream ss;
+        ss << " \"" << junkFromComments << "\",";
+        it.replace(pos + 1, string::npos, ss.str());
+      }
+    }
+  }
+
+  // -- write to file
+  stringstream ss;
+  ss << "rdb/runInfo_" << rr.fBORRunNumber << ".json";
+  ofstream ofs(ss.str());
+  for (const auto &line : newRunInfo) {
+    ofs << line << endl;
+  }
+  ofs.close();
 }
