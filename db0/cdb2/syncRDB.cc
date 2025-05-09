0ce003b1f85c3789fe46610e3670145a79a693f8
Urs Langenegger <urslangenegger@gmail.com>
2025-05-09 17:58:45 +0200

    two modes!

--- db0/cdb2/syncRDB.cc
+++ db0/cdb2/syncRDB.cc
@@ -24,21 +24,24 @@ using namespace std;
 // --
 // -- Usage: bin/syncRunDB -m mode -f firstRun -l lastRun [-t ../../db1/rest/runInfoTemplate.json] [-u localhost:5050/rdb/addAttribute/]
 // --
+// -- -m mode: 0: upload template (magic words: dqTemplate.json or runInfoTemplate.json) to run records
+// --          1: parse shift comments and set runInfo fields "class" and "junk"
 // -- History:
 // --   2025/05/08: first shot
 // ------------------------------------------------------------------------
 
-void rdbMode1(runRecord &);
+void rdbMode1(runRecord &, bool);
+void rdbMode0(runRecord &, bool);
 
 
 string runInfoTemplateFile = "../../db1/rest/runInfoTemplate.json";
+string rdbUpdateString("localhost:5050/rdb/addAttribute");
 vector<string> runInfoTemplateFileLines;
 
 // ----------------------------------------------------------------------
 int main(int argc, char* argv[]) {
 
   // -- command line arguments
-  string rdbUpdateString("localhost:5050/rdb/addAttribute");
   string urlString("localhost:5050/cdb");
 
   bool debug(false);
@@ -76,26 +79,58 @@ int main(int argc, char* argv[]) {
     if ((lastRun > 0) && (irun > lastRun)) continue;
     
     runRecord rr = pDB->getRunRecord(irun);
-    if (1 == mode) {
-      rdbMode1(rr);
-    } 
-    if (!debug) {
-      // curl -X PUT -H "Content-Type: application/json" --data-binary @/Users/ursl/mu3e/mu3eanca/db1/rest/runInfoTemplate.json http://localhost:5050/rdb/addAttribute/513
-      stringstream ss;
-      ss << "curl -X PUT -H \"Content-Type: application/json\" --data-binary @rdb/runInfo_" << irun << ".json " << rdbUpdateString << "/" << irun;
-      cout << "Updating for run number: " << irun << endl; 
-      cout << ss.str() << endl;      
-      system(ss.str().c_str());
-    }
+    if (0 == mode) rdbMode0(rr, debug);
+    if (1 == mode) rdbMode1(rr, debug);
   }
   delete pDB; 
 } 
 
 // ----------------------------------------------------------------------
+// -- upload template {runInfo,dataQuality} to run records 
+// -- IFF they do not yet contain that attribute
+void rdbMode0(runRecord &rr, bool debug) {
+  if (!rr.fBOREORValid) {
+    cout << "incomplete run record in RDB, skipping ................................. " << endl;
+    cout << rr.printString() << endl;
+    return;
+  }
+
+  // -- check whether this is about uploading the dataQuality template and whether that exists
+  if (string::npos != runInfoTemplateFile.find("dqTemplate")) {
+    if (!rr.fDataQualityValid) {
+      cout << "no DataQuality attribute found for run number: " << rr.fBORRunNumber << endl;
+      int irun = rr.fBORRunNumber;
+      stringstream ss;
+      ss << "curl -X PUT -H \"Content-Type: application/json\" --data-binary @" << runInfoTemplateFile << " " << rdbUpdateString << "/" << irun;
+      cout << "Updating for run number: " << irun << endl; 
+      cout << ss.str() << endl;      
+      if (!debug) {
+        system(ss.str().c_str());
+      }
+    }
+  }
+
+  // -- check whether this is about uploading the dataQuality template and whether that exists
+  if (string::npos != runInfoTemplateFile.find("runInfoTemplate")) {
+    if (!rr.fRunInfoValid) {
+      cout << "no runInfo attribute found for run number: " << rr.fBORRunNumber << endl;
+      int irun = rr.fBORRunNumber;
+      stringstream ss;
+      ss << "curl -X PUT -H \"Content-Type: application/json\" --data-binary @" << runInfoTemplateFile << " " << rdbUpdateString << "/" << irun;
+      cout << "Updating for run number: " << irun << endl; 
+      cout << ss.str() << endl;      
+      if (!debug) {
+        system(ss.str().c_str());
+      }
+    }
+  }
+}
+
+// ----------------------------------------------------------------------
 // -- set the following flags by parsing the shift comments
 // -- RunInfo class: source/cosmic/daq
 // -- RunInfo junk:  bad
-void rdbMode1(runRecord &rr) {
+void rdbMode1(runRecord &rr, bool debug) {
 
   vector<string> newRunInfo = runInfoTemplateFileLines;
   
@@ -108,23 +143,34 @@ void rdbMode1(runRecord &rr) {
   cout << "run number: " << rr.fBORRunNumber << ": ";
 
   // -- check for junk indicators stored in vector
-  vector<string> vClassIndicators = {"source", "cosmics", "daq", "calibration", "tuning"};
+  vector<string> vClassIndicators = {"beam", "source", "cosmics", "daq", "calibration", "tuning"};
   for (const auto &indicator : vClassIndicators) {
     if (xstring.find(indicator) != string::npos) {
      cout << " found class indicator: " << indicator;
       classFromComments = indicator;
+      if (indicator == "daq") {
+        junkFromComments = "true";
+      } else if (indicator == "calibration") {
+        junkFromComments = "true";
+      } else if (indicator == "tuning") {
+        junkFromComments = "true";
+      }
       break;
     }
   }
 
   // -- check for junk indicators stored in vector
-  vector<string> vJunkIndicators = {"bad", "error", "problem", "dbx", "fail", "debug", "test", "dummy"};
-  for (const auto &indicator : vJunkIndicators) {
-    if (xstring.find(indicator) != string::npos) {
-     cout << " and junk indicator: " << indicator;
-      junkFromComments = "true";
-      break;
+  if (junkFromComments == "not found") {
+    vector<string> vJunkIndicators = {"bad", "error", "problem", "dbx", "fail", "debug", "test", "dummy"};
+    for (const auto &indicator : vJunkIndicators) {
+      if (xstring.find(indicator) != string::npos) {
+      cout << " and junk indicator: " << indicator;
+        junkFromComments = "true";
+        break;
+      }
     }
+  } else {
+    cout << " and junk indicator: " << junkFromComments;
   }
   cout << endl;
 
@@ -161,4 +207,14 @@ void rdbMode1(runRecord &rr) {
     ofs << line << endl;
   }
   ofs.close();
+
+  if (!debug) {
+    // curl -X PUT -H "Content-Type: application/json" --data-binary @/Users/ursl/mu3e/mu3eanca/db1/rest/runInfoTemplate.json http://localhost:5050/rdb/addAttribute/513
+    int irun = rr.fBORRunNumber;
+    stringstream ss;
+    ss << "curl -X PUT -H \"Content-Type: application/json\" --data-binary @rdb/runInfo_" << irun << ".json " << rdbUpdateString << "/" << irun;
+    cout << "Updating for run number: " << irun << endl; 
+    cout << ss.str() << endl;      
+    system(ss.str().c_str());
+  }
 }
