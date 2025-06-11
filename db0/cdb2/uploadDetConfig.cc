097d31d80e9d35713cb9aa1d3dfc4223ed3d6b69
Urs Langenegger <urslangenegger@gmail.com>
2025-06-11 15:28:54 +0200

    final version also put into mu3eUtil

--- db0/cdb2/uploadDetConfig.cc
+++ db0/cdb2/uploadDetConfig.cc
@@ -1,70 +1,58 @@
-#include <bsoncxx/builder/stream/document.hpp>
-#include <bsoncxx/json.hpp>
-#include <bsoncxx/types.hpp>
-#include <mongocxx/client.hpp>
-#include <mongocxx/instance.hpp>
-#include <mongocxx/uri.hpp>
+
 #include <dirent.h>  /// for directory reading
 #include <fstream>
 #include <iostream>
-#include <vector>
+#include <algorithm>  // for std::sort
+#include <vector>     // for std::vector
+#include <string.h>
+#include <string>     // for std::string
 
-using bsoncxx::builder::stream::document;
-using bsoncxx::builder::stream::finalize;
 using namespace std;
 
-mongocxx::uri gUri;
-mongocxx::client gClient;
 
 // ----------------------------------------------------------------------
-void store_file_in_mongo(const std::string& filepath, const std::string& tag, mongocxx::collection& collection, string dir) {
-  // -- Open the file
-  string fullname = dir + "/" + filepath;
-  cout << "Reading to ->" << fullname << "<-" << endl;
+// -- uploadDetConfig
+// ------------------
+//
+// -- upload (binary) files from a directory with a "tag" to the CDB (detconfigs collection)
+//
+// -- Usage:
+//    uploadDetConfig --dir /Users/ursl/Downloads/tdac_files_bu_06_10 -p mask_chip_ -t tdac_files_bu_06_10
+//
+// -- Retrieval of all files corresponding to "tag" (result will be in a zip file with name "tag".zip)
+//    curl -O -J "http://pc11740:5050/cdb/downloadTag?tag=tdac_files_bu_06_10"
+// ----------------------------------------------------------------------
 
-  ifstream file(fullname, std::ios::binary);
-  if (!file.is_open()) {
-    cout << "Failed to open file: " << filepath << endl;
-    return;
-  }
-  
 
-  vector<uint8_t> buffer((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-  
-  bsoncxx::builder::stream::document document{};
-
-  document << "tag" << tag  // Add tag
-           << "filename" << filepath  // Add filename
-           << "content" << bsoncxx::types::b_binary{
-    bsoncxx::binary_sub_type::k_binary,
-      static_cast<uint32_t>(buffer.size()),
-      buffer.data()
-      };
-
-    
-  
-  // Insert the document into the MongoDB collection
-  collection.insert_one(document.view());
-  
-  cout << "Inserted file: " << filepath << " with tag: " << tag << endl;
-}
 
 // ----------------------------------------------------------------------
 int main(int argc, char* argv[]) {
   
-  string dirPath("."), tag("nada");
+  string dirPath("."), tag("nada"), host("pc11740"), pattern("mask_chip_");
   for (int i = 0; i < argc; i++) {
-    if (!strcmp(argv[i], "--dir")) {dirPath = string(argv[++i]);}
-    if (!strcmp(argv[i], "-dir"))  {dirPath = string(argv[++i]);}
-    if (!strcmp(argv[i], "--uri")) {gUri = bsoncxx::string::view_or_value(argv[++i]); gClient = gUri;}
-    if (!strcmp(argv[i], "-u"))    {gUri = bsoncxx::string::view_or_value(argv[++i]); gClient = gUri;}
-    if (!strcmp(argv[i], "-t"))    {tag = string(argv[++i]);}
-    if (!strcmp(argv[i], "--tag")) {tag = string(argv[++i]);}
+    if (!strcmp(argv[i], "--dir"))     {dirPath = string(argv[++i]);}
+    if (!strcmp(argv[i], "-d"))        {dirPath = string(argv[++i]);}
+    if (!strcmp(argv[i], "--host"))    {host = string(argv[++i]);}
+    if (!strcmp(argv[i], "-h"))        {host = string(argv[++i]);}
+    if (!strcmp(argv[i], "-t"))        {tag = string(argv[++i]);}
+    if (!strcmp(argv[i], "--tag"))     {tag = string(argv[++i]);}
+    if (!strcmp(argv[i], "--pattern")) {pattern = string(argv[++i]);}
+    if (!strcmp(argv[i], "-p"))        {pattern = string(argv[++i]);}
   }
 
-  // Access the database and collection
-  auto db = gClient["mu3e"];
-  mongocxx::collection collection = db["detconfigs"];
+    // -- check if tag is specified
+  if ("nada" == tag) {
+    cout << "Tag is not specified" << endl;
+    cout << "Usage: uploadDetConfig --dir /path/to/directory -p mask_chip_ -t tag" << endl;
+    return 1;
+  }
+
+
+  // -- Access the database and collection
+  // string url = "http://" + host + ":5050/cdb/uploadMany";
+  string url = "http://" + host + ":5050/cdb/upload";
+  cout << "Uploading to ->" << url << "<-" << endl;
+  string cmd = "curl -v -F ... -F \"tag=4\" " + url;
   
   vector<string> vfiles;
   DIR *folder;
@@ -73,11 +61,15 @@ int main(int argc, char* argv[]) {
   folder = opendir(dirPath.c_str());
   if (folder == NULL) {
     cout << "Unable to read directory ->" << dirPath << "<-" << endl;
+    cout << "Usage: uploadDetConfig --dir /path/to/directory -p mask_chip_ -t tag" << endl;
+    return 1;
   } else {
   
     while ((entry=readdir(folder))) {
       if (8 == entry->d_type) {
-        vfiles.push_back(entry->d_name);
+        if (string(entry->d_name).find(pattern) != string::npos) {  
+          vfiles.push_back(entry->d_name);
+        }
       }
     }
     closedir(folder);
@@ -86,7 +78,9 @@ int main(int argc, char* argv[]) {
   
   // -- Iterate over the files and store them in MongoDB
   for (const auto& filepath : vfiles) {
-    store_file_in_mongo(filepath, tag, collection, dirPath);
+    string cmd = "curl -v -F \"file=@" + dirPath + "/" + filepath + "\" -F \"tag=" + tag + "\" " + url;
+    cout << "cmd: " << cmd << endl;
+    system(cmd.c_str());
   }
   
   return 0;
