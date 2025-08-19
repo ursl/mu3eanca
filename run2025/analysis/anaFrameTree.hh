ffbda647abaa427d3d625f9da158049eaf3dd3d7
Urs Langenegger <urslangenegger@gmail.com>
2025-08-19 23:41:40 +0200

    new analysis class for frameTree

---
+++ run2025/analysis/anaFrameTree.hh
@@ -0,0 +1,63 @@
+#ifndef anaFrameTree_h
+#define anaFrameTree_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+// Header file for the classes stored in the TTree if any.
+
+class anaFrameTree {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+   // Declaration of leaf types
+   Int_t           run;
+   UInt_t          frameID;
+   Int_t           hitN;
+   Int_t           hitPixelID[10000];   //[hitN]
+   Int_t           hitToT[10000];   //[hitN]
+   ULong64_t       hitDebugSiData[10000];   //[hitN]
+   Int_t           hitChipID[10000];   //[hitN]
+   Int_t           hitCol[10000];   //[hitN]
+   Int_t           hitRow[10000];   //[hitN]
+   Int_t           hitTime[10000];   //[hitN]
+   Int_t           hitTimeNs[10000];   //[hitN]
+   Int_t           hitRawToT[10000];   //[hitN]
+   Int_t           hitBitToT[10000];   //[hitN]
+   Int_t           hitStatus[10000];   //[hitN]
+   Int_t           hitStatusBits[10000];   //[hitN]
+
+   // List of branches
+   TBranch        *b_run;   //!
+   TBranch        *b_frameID;   //!
+   TBranch        *b_hitN;   //!
+   TBranch        *b_hitPixelID;   //!
+   TBranch        *b_hitToT;   //!
+   TBranch        *b_hitDebugSiData;   //!
+   TBranch        *b_hitChipID;   //!
+   TBranch        *b_hitCol;   //!
+   TBranch        *b_hitRow;   //!
+   TBranch        *b_hitTime;   //!
+   TBranch        *b_hitTimeNs;   //!
+   TBranch        *b_hitRawToT;   //!
+   TBranch        *b_hitBitToT;   //!
+   TBranch        *b_hitStatus;   //!
+   TBranch        *b_hitStatusBits;   //!
+
+   anaFrameTree(TTree *tree=0);
+   virtual ~anaFrameTree();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual bool     Notify();
+   virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
