b0bfecea67a843c2a13513ebad9f9dec12ea818a
Urs Langenegger <urslangenegger@gmail.com>
2025-10-16 15:31:04 +0200

    executable to extract FED/Link/Matrix mapping from mu3e_midas_meta json output

---
+++ run2025/analysis/fedLinkMatrixMap.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <math.h>
+#include <string>
+
+
+#include <iterator>
+
+#include "util.hh"
+
+using namespace std;
+
+// ----------------------------------------------------------------------
+int main(int argc, char *argv[]) {
+
+  return 0;
+}
