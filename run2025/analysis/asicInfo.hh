d9a0ab4e0f2e0307c0a5f9583fc2a59e3f861144
Urs Langenegger <urslangenegger@gmail.com>
2025-10-21 11:08:41 +0200

    move out for analysis (tree dumper)

---
+++ run2025/analysis/asicInfo.hh
@@ -0,0 +1,28 @@
+#ifndef ASICINFO_h
+#define ASICINFO_h  
+// ----------------------------------------------------------------------
+struct AsicInfo {
+  int confId{};
+  int globalId{};
+  int fedID{};
+  int idxInSection{}; // position within the section (0..)
+  std::string linkMask;
+  std::string linkMatrix;
+  long long lvdsErrRate0{};
+  long long lvdsErrRate1{};
+  long long lvdsErrRate2{};
+  int ckdivend{};
+  int ckdivend2{};
+  int vdacBLPix{};
+  int vdacThHigh{};
+  int vdacThLow{};
+  int biasVNOutPix{};
+  int biasVPDAC{};
+  int biasVNDcl{};
+  int biasVNLVDS{};
+  int biasVNLVDSDel{};
+  int biasVPDcl{};
+  int biasVPTimerDel{};
+  int vdacBaseline{};
+};
+#endif
