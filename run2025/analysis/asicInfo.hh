19927401b5642935266421893e3a9812fd766fc9
Urs Langenegger <urslangenegger@gmail.com>
2025-10-28 21:51:38 +0100

    type changes

--- run2025/analysis/asicInfo.hh
+++ run2025/analysis/asicInfo.hh
@@ -6,14 +6,17 @@ struct AsicInfo {
   int globalId{};
   int fedID{};
   int idxInSection{}; // position within the section (0..)
+  std::string FEBLinkName;
   std::string linkMask;
   std::string linkMatrix;
   long long lvdsErrRate0{};
   long long lvdsErrRate1{};
   long long lvdsErrRate2{};
   // -- ABC link information (the above resorted with linkMask)
-  int abcLinkMask[3]{0,0,0};
-  int abcLinkMaxLvdsErrRate[3]{0,0,0};
+  int         abcLinkMask[3]{0,0,0};
+  long long   abcLinkErrs[3]{0LL,0LL,0LL};
+  std::string abcLinkNames[3]{"unset", "unset", "unset"};
+  int         abcLinkOffsets[3]{9,9,9};
   int ckdivend{};
   int ckdivend2{};
   int vdacBLPix{};
