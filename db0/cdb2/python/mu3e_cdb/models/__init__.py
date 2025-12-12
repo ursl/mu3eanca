2135d2f5c7efdc40f48c9615b3da5b664124f4bd
Urs Langenegger <urslangenegger@gmail.com>
2025-12-12 10:18:20 +0100

    this has moved elsewhere

--- db0/cdb2/python/mu3e_cdb/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Data models for CDB."""
-
-from .payload import Payload
-from .run_record import RunRecord
-from .config import ConfigPayload
-
-__all__ = ["Payload", "RunRecord", "ConfigPayload"]
-
