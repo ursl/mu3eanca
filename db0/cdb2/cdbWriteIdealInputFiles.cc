51ae76efca5b96eee895eeed5a1e962f669863b6
Urs Langenegger <urslangenegger@gmail.com>
2026-03-16 15:29:25 +0100

    remove TS dbx code

--- db0/cdb2/cdbWriteIdealInputFiles.cc
+++ db0/cdb2/cdbWriteIdealInputFiles.cc
@@ -11,20 +11,8 @@ using namespace std;
 //
 // ----------------------------------------------------------------------
 
-void callX() {
-  cout << "Calling X" << endl;
-  double eff(0.);
-  for (int i = 0; i < 800; i++) {
-    for (int j = 1; j < 10000; j++) {
-      eff = double(i)/double(j);
-      // 39.258688
-      if (eff > 0.392586 && eff < 0.392587) {
-        cout << "i: " << i << ", j: " << j << ", eff: " << eff << endl;
-      }
-    }
-  }
-}
 
+// ----------------------------------------------------------------------
 int main(int argc, const char* argv[]) {
   // -- command line arguments 
   string mode("pixeltimecalibration");
@@ -33,7 +21,6 @@ int main(int argc, const char* argv[]) {
   for (int i = 0; i < argc; i++) {
     if (!strcmp(argv[i], "-m")) {  mode = argv[++i];  }
     if (!strcmp(argv[i], "-f")) {  filename = argv[++i];  }
-    if (!strcmp(argv[i], "-x")) { callX(); return 0;}
   }
 
   cdbPayloadWriter writer;
