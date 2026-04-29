6e843e81a6afcc7a17ffbc33f275ceabd6bd056c
Urs Langenegger <urslangenegger@gmail.com>
2026-04-29 13:42:19 +0200

    add annotation

--- prompt/relval/ana/runFillHistograms.cc
+++ prompt/relval/ana/runFillHistograms.cc
@@ -12,6 +12,7 @@ int main(int argc, char **argv) {
   string infile = "";
   string tree = "frames";
   string mode = "relval";
+  string annotation = "";
   int nevents = -1;
   for (int i = 1; i < argc; ++i) {
     if (!strcmp(argv[i], "--mode")) { mode = argv[++i]; }
@@ -20,15 +21,16 @@ int main(int argc, char **argv) {
     if (!strcmp(argv[i], "--in")) { infile = argv[++i]; }
     if (!strcmp(argv[i], "--out")) { outfile = argv[++i]; }
     if (!strcmp(argv[i], "--outdir")) { outdir = argv[++i]; }
+    if (!strcmp(argv[i], "--ann")) { annotation = argv[++i]; }
     if (!strcmp(argv[i], "--tree")) { tree = argv[++i]; }
   }
 
   if (outfile == "nada") {
     string baseinfilename = infile.substr(infile.find_last_of('/') + 1);
-    outfile = "histograms-" + baseinfilename.substr(0, baseinfilename.find_last_of('.')) + ".root";
+    baseinfilename = baseinfilename.substr(0, baseinfilename.find_last_of('.'));
+    outfile = "histograms-" + (annotation != "" ? annotation + "-": "") + baseinfilename + ".root";
     outfile = outdir + "/" + outfile;
     cout << "runFillHistograms() INFO: output file not specified, using default " << outfile << endl;
-    return 0; 
   }
 
   fillHist fhFrames(infile, outfile);
