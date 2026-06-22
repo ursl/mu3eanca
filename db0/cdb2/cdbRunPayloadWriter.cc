35dd6db1c5906067e1b57770b83cfaeb6a037492
Urs Langenegger <urslangenegger@gmail.com>
2026-06-22 16:21:10 +0200

    more documentation and variable name change

--- db0/cdb2/cdbRunPayloadWriter.cc
+++ db0/cdb2/cdbRunPayloadWriter.cc
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <string.h>
 
+#include <dirent.h>
+
+#include <string>
+
 using namespace std;
 
 // ----------------------------------------------------------------------
@@ -19,6 +23,10 @@ using namespace std;
 // Usage examples
 // ./bin/cdbRunPayloadWriter -m 2025 -c fibrealignment -f ascii/mu3e_alignment_mcidealv6.5.root -a "as installed in 2025" -p ~/data/mu3e/cdb/payloads -g datav6.5=2025V1
 // ./bin/cdbRunPayloadWriter -t datav6.9=2025V0 -a "all frames with perfect data for all subsystems" -p payloads -d /Users/ursl/data/mu3e/run2025/rawv2 -r 1 -c eventstuffv2
+//
+// ./bin/cdbRunPayloadWriter -m eventstuffv2-ideal -f eventstuff-ideal-test.json
+// ./bin/cdbRunPayloadWriter -c eventstuffv2 -a "ideal tag, for inclusion of skipped header bug for pixels" -t mcidealv6.9 -r 1 
+// ./bin/cdbRunPayloadWriter -c eventstuffv2 -d ~/data/mu3e/run2025/rawv2 -a "includes skipped header bug for pixels, but nothing on fibres of tiles" -t datav6.9=2025V0 
 // 
 // ----------------------------------------------------------------------
 
@@ -31,13 +39,13 @@ int main(int argc, const char* argv[]) {
 
   string uri("/Users/ursl/data/mu3e/test-cdb");
   string tagname("unset");
-  string binmaskfiledir("unset");
+  string inputfiledir("unset");
   string annotation("unset");
   int run(1);
   
   for (int i = 0; i < argc; i++) {
     if (!strcmp(argv[i], "-a")) {  annotation = argv[++i];  }
-    if (!strcmp(argv[i], "-d")) {  binmaskfiledir = argv[++i];  }
+    if (!strcmp(argv[i], "-d")) {  inputfiledir = argv[++i];  }
     if (!strcmp(argv[i], "-f")) {  filename = argv[++i];  }
     if (!strcmp(argv[i], "-m")) {  mode = argv[++i];  }
     if (!strcmp(argv[i], "-r")) {  run = atoi(argv[++i]);  }
@@ -91,21 +99,21 @@ int main(int argc, const char* argv[]) {
   
   // -- pixelmask
   if (mode.find("pixelmask") != string::npos) {
-    if (run > 1 && binmaskfiledir == "unset") {
-      cout << "Error: binmaskfiledir is unset" << endl;
-      cout << "Usage: cdbRunPayloadWriter -u <CDBPATH> -m pixelmask -d <binmaskfiledir>" << endl;
+    if (run > 1 && inputfiledir == "unset") {
+      cout << "Error: inputfiledir is unset" << endl;
+      cout << "Usage: cdbRunPayloadWriter -u <CDBPATH> -m pixelmask -d <inputfiledir>" << endl;
       return 0;
     }
     if (uri == "unset") {
       cout << "Error: uri is unset" << endl;
-      cout << "Usage: cdbRunPayloadWriter -u <CDBPATH> -m pixelmask -d <binmaskfiledir>" << endl;
+      cout << "Usage: cdbRunPayloadWriter -u <CDBPATH> -m pixelmask -d <inputfiledir>" << endl;
       return 0;
     }
     if (run == 1) {
       writer.writePixelMaskIdealPayload(uri+"/payloads/", tagname, annotation, mode);
      return 0;
     } else {
-      writer.writePixelMaskPayloads(uri+"/payloads/", tagname, binmaskfiledir, annotation, run);
+      writer.writePixelMaskPayloads(uri+"/payloads/", tagname, inputfiledir, annotation, run);
       return 0;
     }
   }
@@ -117,7 +125,7 @@ int main(int argc, const char* argv[]) {
       cout << "Usage: cdbRunPayloadWriter -m eventstuffv2-ideal -f filename" << endl;
       return 0;
     }
-    writer.writeEventStuffV2IdealPayload(uri+"/payloads/", tagname, filename, annotation, run);
+    writer.writeEventStuffV2IdealInput(filename, mode);
     return 0;
   }
 
